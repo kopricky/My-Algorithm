d56768c58dd6b1cbbd126849c2f8f73cf9b22ea9
kopricky <kopricky@yahoo.co.jp>
2017-11-14 14:18:25 +0900

    revise

--- competitive_programming/computation/prime_number.cpp
+++ competitive_programming/computation/prime_number.cpp
@@ -1,19 +1,18 @@
 vector<int> prime;
 bool is_prime[MAX_N];
 
-int sieve(int n)
-{
+void sieve(int n){
 	for(int i=0;i<=n;i++){
 		is_prime[i] = true;
 	}
 	is_prime[0] = is_prime[1] = false;
-	for(int i=2;i<=n;i++){
+	prime.push_back(2);
+	for(int i=3;i<=n;i+=2){
 		if(is_prime[i]){
-			prime.pb(i);
+			prime.push_back(i);
 			for(int j=2*i;j<=n;j+=i){
 				is_prime[j] = false;
 			}
 		}
 	}
-	return p;
 }
