059cd48ef1a4c0de18c8d6623713c7f5930ea50e
kopricky <kopricky@yahoo.co.jp>
2017-11-14 14:40:48 +0900

    revise

--- competitive_programming/computation/miller_rabbin.cpp
+++ competitive_programming/computation/miller_rabbin.cpp
@@ -1,11 +1,12 @@
 struct Miller{
-    const vector<ll> v = {2,7,61}; // < 4,759,123,141
-    //const vector<ll> v = {2,3,5,7,11,13,17}; // < 341,550,071,728,321
+    const vector<ll> v = {2,7,61}; // < 4,759,123,141 ≒ 4×10^9
+    //const vector<ll> v = {2,3,5,7,11,13,17}; < 341,550,071,728,321 ≒　3*10^14
+    //const vector<ll> v = {2,3,5,7,11,13,17,19,23,29,31,37} 2^64までいける
     // x^k (mod m)
-    ll modpow( ll x, ll k, ll m ){
+    ll modpow(ll x, ll k, ll m){
         ll res = 1;
-        while( k ){
-          if( k & 1 ){
+        while(k){
+          if(k & 1){
             res = res * x % m;
           }
           k /= 2;
@@ -14,29 +15,29 @@ struct Miller{
         return res;
     }
     // check if n is prime
-    bool check( ll n ){
-        if( n < 2 ){
+    bool check(ll n){
+        if(n < 2){
           return false;
         }
         ll d = n - 1;
         ll s = 0;
-        while( d % 2 == 0 ){
+        while(d % 2 == 0){
             d /= 2;
             s++;
         }
-        for( ll a : v ){
-            if( a == n ){
+        for(ll a : v){
+            if(a == n){
               return true;
             }
-            if( modpow( a , d , n ) != 1 ){
+            if(modpow(a,d,n) != 1){
               bool ok = true;
-              for( ll r = 0; r < s; r++ ){
-                if( modpow( a, d * (1LL << r), n ) == n-1 ){
+              for(ll r=0;r<s;r++){
+                if( modpow(a,d*(1LL << r),n) == n-1){
                   ok = false;
                   break;
                 }
               }
-              if( ok ){
+              if(ok){
                 return false;
               }
             }
