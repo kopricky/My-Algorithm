ec13021c7944d06291d7d4880d6c6a6d3a0dbed2
kopricky <kopricky@yahoo.co.jp>
2017-11-15 20:50:38 +0900

    revise

--- competitive_programming/data_structure/segtree.cpp
+++ competitive_programming/data_structure/segtree.cpp
@@ -1,40 +1,50 @@
-const int N = 1 << 17;	//10^5程度
-
-int n_,dat[2*N-1];
-
-//index:iがdat[n_-1+i]と対応
-void init(int x)
-{
-	n_=1;
-	while(n_<x){
-		n_*=2;
-	}
-	rep(i,2*n_-1){
-		dat[i] = INF;
-	}
-}
-
-void update(int k,int a)
-{
-	k += n_-1;
-	dat[k] = a;
-	while(k>0){
-		k = (k-1)/2;
-		dat[k] = min(dat[2*k+1],dat[2*k+2]);
-	}
-}
-
-//外からはquery(a,b,0,0,n_)として呼ぶ。(n_とすることに注意)
-int query(int a,int b,int k,int l,int r)
-{
-	if(r <= a || b <= l){
-		return INF;
-	}
-	if(a <= l && r <= b){
-		return dat[k];
-	}else{
-		int vl = query(a,b,2*k+1,l,(l+r)/2);
-		int vr = query(a,b,2*k+2,(l+r)/2,r);
-		return min(vl,vr);
-	}
-}
+template<typename V> class segtree {
+private:
+    int n,sz;
+    vector<V> node;
+public:
+    segtree(vector<V>& v){
+        sz = (int)v.size();
+        n = 1;
+        while(n < sz){
+            n *= 2;
+        }
+        node.resize(2*n-1);
+        rep(i,sz){
+            node[i+n-1] = v[i];
+        }
+        for(int i=n-2; i>=0; i--){
+            node[i] = min(node[i*2+1],node[i*2+2]);
+        }
+    }
+    void update(int k,int a)
+    {
+    	k += n-1;
+    	node[k] = a;
+    	while(k>0){
+    		k = (k-1)/2;
+    		node[k] = min(node[2*k+1],node[2*k+2]);
+    	}
+    }
+    V query(int a,int b,int k=0,int l=0,int r=-1)
+    {
+        if(r < 0) r = n;
+    	if(r <= a || b <= l){
+    		return numeric_limits<V>::max();
+    	}
+    	if(a <= l && r <= b){
+    		return node[k];
+    	}else{
+    		V vl = query(a,b,2*k+1,l,(l+r)/2);
+    		V vr = query(a,b,2*k+2,(l+r)/2,r);
+    		return min(vl,vr);
+    	}
+    }
+    void print()
+    {
+        rep(i,sz){
+            cout << query(i,i+1) << " ";
+        }
+        cout << endl;
+    }
+};
