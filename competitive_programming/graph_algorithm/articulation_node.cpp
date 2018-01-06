06f53f0f65bbe678ac4357ddbd8135342913203d
kopricky <kopricky@yahoo.co.jp>
2018-01-06 10:22:23 +0900

    revise

--- competitive_programming/graph_algorithm/articulation_node.cpp
+++ competitive_programming/graph_algorithm/articulation_node.cpp
@@ -1,15 +1,24 @@
 //頂点数がMAX_N
 //Articulation at(MAX_N);
 //at.artに関節点が入る
+typedef pair<int,int>P;
+
 class Articulation {
-	using P = pair<int,int>;
 public:
 	vector<vector<int> > G;
 	vector<int> ord;	//訪問時間
 	vector<int> low; //low[u]はuから高々1個の後退辺を通ってたどりつけるノードのordの最小値
 	vector<bool> visit; //訪問したかどうかのフラグ
-	vector<int> art;	//関節点を格納
+	vector<bool> art;	//関節点かどうかの判定
 	int V;
+	Articulation(int node_size){
+		V = node_size;
+		G.resize(node_size),art.resize(node_size.false);
+		ord.resize(node_size),low.resize(node_size),visit.resize(node_size,false);
+	}
+	void add_edge(int a,int b){
+		G[a].push_back(b),G[b].push_back(a);
+	}
 	void dfs(int v,int p,int &k){
 		visit[v] = true;
 		ord[v] = low[v] = k++;
@@ -20,20 +29,17 @@ public:
 				dfs(G[v][i],v,k);
 				low[v] = min(low[v],low[G[v][i]]);	//子のノードのlowと比較する
 				if(p >= 0 && ord[v] <= low[G[v][i]]){
-					art.push_back(v);
+					art[v] = true;
 				}
 			}else if(G[v][i] != p && ord[G[v][i]] < low[v]){
 				low[v] = ord[G[v][i]];
 			}
 		}
 		if(p == -1 && ct > 1){
-			art.push_back(v);	//根の次数が2以上なら根は関節点
+			art[v] = true;	//根の次数が2以上なら根は関節点
 		}
 	}
-	Articulation(int node_size){
-		V = node_size;
-		G.resize(node_size);
-		ord.resize(node_size),low.resize(node_size),visit.resize(node_size,false);
+	void solve(){
 		for(int i=0;i<node_size;i++){
 			int k = 0;
 			if(!visit[i]){
@@ -41,7 +47,7 @@ public:
 			}
 		}
 	}
-	void add_edge(int a,int b){
-		G[a].push_back(b),G[b].push_back(a);
+	bool isarticulation(int node_id){
+		return art[node_id];
 	}
 };
