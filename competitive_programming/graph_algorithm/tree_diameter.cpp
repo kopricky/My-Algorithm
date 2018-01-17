737995809055df4725f15757dd8e5a23a04929ba
kopricky <kopricky@yahoo.co.jp>
2018-01-18 03:58:09 +0900

    revise

---
+++ competitive_programming/graph_algorithm/tree_diameter.cpp
@@ -0,0 +1,50 @@
+class TreeDiameter
+{
+public:
+    vector<vector<int> > G;
+    vector<int> diameter;
+    int V;
+    TreeDiameter(int node_size){
+        V = node_size;
+        G.resize(V);
+    }
+    void add_edge(int u,int v){
+        G[u].push_back(v),G[v].push_back(u);
+    }
+    void dfs(int u,int p,int d,int& far,int& mx){
+        if(mx < d){
+            far = u;
+            mx = d;
+        }
+        for(int v : G[u]){
+            if(v != p){
+                dfs(v,u,d+1,far,mx);
+            }
+        }
+    }
+    bool redfs(int u,int p,const int t){
+        if(u == t){
+            return true;
+        }
+        for(int v : G[u]){
+            if(v != p){
+                diameter.push_back(v);
+                if(redfs(v,u,t)){
+                    return true;
+                }else{
+                    diameter.pop_back();
+                }
+            }
+        }
+        return false;
+    }
+    void solve(){
+        int s,t,mx;
+        mx = -1;
+        dfs(0,-1,0,s,mx);
+        mx = -1;
+        dfs(s,-1,0,t,mx);
+        diameter.push_back(s);
+        redfs(s,-1,t);
+    }
+};
