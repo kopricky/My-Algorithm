dcbb2a2a1940408b72180766e35bf653df3df978
kopricky <kopricky@yahoo.co.jp>
2018-02-23 13:43:41 +0900

    revise

--- competitive_programming/other/template.cpp
+++ competitive_programming/other/template.cpp
@@ -6,7 +6,7 @@
 #define rep(i,n) for(int i=0;i<(int)(n);++i)
 #define rrep(i,n) for(int i=(int)(n)-1;i>=0;--i)
 #define srep(i,s,t) for(int i=(int)(s);i<(int)(t);++i)
-#define each(a,b) for(auto (a): (b))
+#define each(a,b) for(auto& (a): (b))
 #define all(v) (v).begin(),(v).end()
 #define len(v) (int)(v).size()
 #define zip(v) sort(all(v)),v.erase(unique(all(v)),v.end())
@@ -17,7 +17,9 @@
 #define pb push_back
 #define show(x) cout<<#x<<" = "<<(x)<<endl
 #define spair(p) cout<<#p<<": "<<p.fi<<" "<<p.se<<endl
+#define sar(a,n) cout<<#a<<":";rep(kbrni,n)cout<<" "<<a[kbrni];cout<<endl
 #define svec(v) cout<<#v<<":";rep(kbrni,v.size())cout<<" "<<v[kbrni];cout<<endl
+#define svecp(v) cout<<#v<<":";each(kbrni,v)cout<<" {"<<kbrni.first<<":"<<kbrni.second<<"}";cout<<endl
 #define sset(s) cout<<#s<<":";each(kbrni,s)cout<<" "<<kbrni;cout<<endl
 #define smap(m) cout<<#m<<":";each(kbrni,m)cout<<" {"<<kbrni.first<<":"<<kbrni.second<<"}";cout<<endl
 
@@ -28,6 +30,7 @@ typedef pair<ll,ll> pll;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef vector<ll> vl;
+typedef vector<vl> vvl;
 typedef vector<double> vd;
 typedef vector<P> vp;
 typedef vector<string> vs;
