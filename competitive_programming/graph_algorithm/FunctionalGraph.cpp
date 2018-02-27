380f60deda860564511133de547c8e599e4cc7ea
kopricky <kopricky@yahoo.co.jp>
2018-02-27 16:13:54 +0900

    revise

--- competitive_programming/graph_algorithm/FunctionalGraph.cpp
+++ competitive_programming/graph_algorithm/FunctionalGraph.cpp
@@ -1,22 +1,22 @@
 //すべての頂点の出次数が1のグラフ(自己ループはない)
+//ループの番号はすべて負の数
 class FunctionalGraph {
 public:
-    vector<vector<int> > loop; //ループは負数
-    vector<int> nx,start,deg,visit; //stはサイクルから生えた鎖のスタートの頂点
+    vector<vector<int> > loop;
+    vector<int> nx,visit; //stはサイクルから生えた鎖のスタートの頂点
     int V;
     FunctionalGraph(int node_size){
         V = node_size;
-        nx.resize(V,0),deg.resize(V,0),visit.resize(V,0);
+        nx.resize(V,0),visit.resize(V,0);
     }
     void add_edge(int u,int v){
         nx[u] = v;
-        deg[v]++;
     }
     void make_loop(const int st,int nw,int ch,vector<int>& vec){
         while(nx[nw] != st){
             vec.push_back(nx[nw]);
             visit[nx[nw]] = ch;
-            break;
+            nw = nx[nw];
         }
     }
     void dfs(int u,int kind,vector<int>& vec){
@@ -34,9 +34,6 @@ public:
     void solve(){
         int id = 1;
         rep(i,V){
-            if(deg[i] == 0){
-                start.push_back(i);
-            }
             if(visit[i] == 0){
                 vector<int> vec;
                 dfs(i,id++,vec);
