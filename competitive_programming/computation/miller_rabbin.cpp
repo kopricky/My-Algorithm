2b81ba34ae014d382ef82de578b17f05040d0580
kopricky <kopricky@yahoo.co.jp>
2018-04-02 03:44:29 +0900

    revise

--- competitive_programming/computation/miller_rabbin.cpp
+++ competitive_programming/computation/miller_rabbin.cpp
@@ -1,5 +1,5 @@
 //動作はint型のみ保証
-//long long型の二乗とかが怪しい
+//long long型の二乗とかが怪しい(int_128とか使ったらいけるかも)
 struct Miller{
     const vector<ll> v = {2,7,61}; // < 4,759,123,141 ≒ 4×10^9
     //const vector<ll> v = {2,3,5,7,11,13,17}; < 341,550,071,728,321 ≒　3*10^14
@@ -8,11 +8,11 @@ struct Miller{
     ll modpow(ll x, ll k, ll m){
         ll res = 1;
         while(k){
-          if(k & 1){
-            res = res * x % m;
-          }
-          k /= 2;
-          x = x * x % m;
+            if(k & 1){
+                res = res * x % m;
+            }
+            k /= 2;
+            x = x * x % m;
         }
         return res;
     }
@@ -32,16 +32,16 @@ struct Miller{
               return true;
             }
             if(modpow(a,d,n) != 1){
-              bool ok = true;
-              for(ll r=0;r<s;r++){
-                if(modpow(a,d*(1LL << r),n) == n-1){
-                  ok = false;
-                  break;
+                bool ok = true;
+                for(ll r=0;r<s;r++){
+                    if(modpow(a,d*(1LL << r),n) == n-1){
+                        ok = false;
+                        break;
+                    }
                 }
-              }
-              if(ok){
-                return false;
-              }
+                if(ok){
+                    return false;
+                }
             }
         }
         return true;
