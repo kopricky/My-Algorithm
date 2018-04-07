6b9f3b0acef62755d35c6af50253d6940cf0bebb
kopricky <kopricky@yahoo.co.jp>
2018-04-08 03:11:38 +0900

    revise

--- competitive_programming/classic_problem/LPS.cpp
+++ competitive_programming/classic_problem/LPS.cpp
@@ -1,44 +1,22 @@
 //最長部分回文列を求める(O(n^2))
-string LPS(const string& s)
+int LPS(const string& s)
 {
     int n = (int)s.size();
     vector<vector<int> > dp(n+1,vector<int>(n+1,0));
-    rep(i,n+1){
-        dp[i][0] = dp[0][i] = i;
-    }
     rep(i,n){
-        rep(j,n-1-i){
-            dp[i+1][j+1] = (s[i]==s[n-1-j])?dp[i][j]:min(dp[i][j+1]+1,dp[i+1][j]+1);
+        dp[i][i] = 1;
+    }
+    for(int i = 2; i <= n; i++){
+        rep(j,n-i+1){
+            int k = i + j - 1;
+            if(s[j] == s[k] && i == 2){
+                dp[j][k] = 2;
+            }else if (s[j] == s[k]){
+                dp[j][k] = dp[j+1][k-1] + 2;
+            }else{
+                dp[j][k] = max(dp[j][k-1], dp[j+1][k]);
+            }
         }
     }
-    int mn = n, x = 0, y = n;
-    rep(i,n+1){
-        if(mn > dp[i][n-i]){
-            mn = dp[i][n-i];
-            x = i, y = n-i;
-        }
-    }
-    string mid;
-    rep(i,n){
-        if(mn > dp[i][n-1-i]){
-            mn = dp[i][n-1-i];
-            x = i, y = n-i-1;
-            mid = s[i];
-        }
-    }
-    string res;
-    while(x > 0 && y > 0){
-        int m = min({dp[x-1][y-1],dp[x-1][y],dp[x][y]});
-        if(m == dp[x-1][y-1]){
-            res.push_back(s[x-1]);
-            x--, y--;
-        }else if(m == dp[x-1][y]){
-            x--;
-        }else{
-            y--;
-        }
-    }
-    string rres = res;
-    reverse(rres.begin(),rres.end());
-    return rres + mid + res;
+    return dp[0][n-1];
 }
