b0140268959f2b815f97ee2857e51269dd1e166a
kopricky <kopricky@yahoo.co.jp>
2018-04-18 22:29:54 +0900

    revise

--- competitive_programming/header.hpp
+++ competitive_programming/header.hpp
@@ -4,6 +4,9 @@
 #include <set>
 #include <map>
 #include <numeric>
+#include <queue>
+#include <stack>
+#include <deque>
 #define rep(i,n) for(int i = 0; i < (int)n; ++i)
 
 using namespace std;
--- competitive_programming/other/EraseOuterInnerInterval.hpp
+++ competitive_programming/other/EraseOuterInnerInterval.hpp
@@ -1,55 +1,60 @@
+#include "../header.hpp"
+
 //区間(pair<int,int>)の配列が与えられたときに最内の区間および最外の区間を取り除く(前処理で用いる)
 //外側の区間を削除
-typedef pair<int,int> P;
-vector<P> EraseOuterInterval(vector<P> vec)
+template<typename T>
+vector<pair<T, T> > EraseOuterInterval(vector<pair<T, T> > vec)
 {
-    vector<P> res;
-    sort(all(vec),[](const P& a,const P& b){
+    using ptt = pair<T, T>;
+    vector<ptt> res;
+    sort(all(vec),[](const ptt& a,const ptt& b){
         return (a.second == b.second)?(a.first > b.first):(a.second < b.second);
     });
-    priority_queue<P> que;
+    priority_queue<ptt> que;
     for(int i = (int)vec.size()-1; i >= 0; i--){
         while(!que.empty()){
-            P p = que.top();
+            ptt p = que.top();
             if(-p.first > vec[i].first){
                 break;
             }
             que.pop();
         }
-        que.push(P(-vec[i].first,vec[i].second));
+        que.push(ptt(-vec[i].first,vec[i].second));
     }
     while(!que.empty()){
         int x,y;
         tie(x,y) = que.top();
         que.pop();
-        res.push_back(P(-x,y));
+        res.push_back(ptt(-x,y));
     }
     return res;
 }
 //内側の区間を削除
-vector<P> EraseInnerInterval(vector<P> vec)
+template<typename T>
+vector<pair<T, T> > EraseInnerInterval(vector<pair<T, T> > vec)
 {
-    vector<P> res;
-    sort(all(vec),[](const P& a,const P& b){
+    using ptt = pair<T, T>;
+    vector<ptt> res;
+    sort(all(vec),[](const ptt& a,const ptt& b){
         return (a.second == b.second)?(a.first < b.first):(a.second > b.second);
     });
-    priority_queue<P> que;
+    priority_queue<ptt> que;
     for(int i = (int)vec.size()-1; i >= 0; i--){
         while(!que.empty()){
-            P p = que.top();
+            ptt p = que.top();
             if(p.first < vec[i].first){
                 break;
             }
             que.pop();
         }
-        que.push(P(vec[i].first,vec[i].second));
+        que.push(ptt(vec[i].first,vec[i].second));
     }
     vec.clear();
     while(!que.empty()){
         int x,y;
         tie(x,y) = que.top();
         que.pop();
-        res.push_back(P(x,y));
+        res.push_back(ptt(x,y));
     }
     return res;
 }
