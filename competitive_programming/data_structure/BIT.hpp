a421cdc70dc865e5b1fa100bdd420fc0efbd9237
Masataka Gohda <masataka-goda@g.ecc.u-tokyo.ac.jp>
2018-04-23 02:10:00 +0900

    Revert "revise"
    This reverts commit 4926e0797f27ae16b5831e9015a11687b94943b5.

--- competitive_programming/data_structure/BIT.hpp
+++ competitive_programming/data_structure/BIT.hpp
@@ -1,13 +1,13 @@
 #include "../header.hpp"
 
-template<typename T> class BIT {
+template<typename V> class BIT {
 private:
-	int n; vector<T> bit;
+	int n; vector<V> bit;
 public:
-	void add(int i,T x){ i++; while(i < n) bit[i] += x, i += i & -i;}
-	T sum(int i){ i++; T s = 0; while(i>0) s += bit[i], i -= i & -i; return s;}
+	void add(int i,V x){ i++; while(i < n) bit[i] += x, i += i & -i;}
+	V sum(int i){ i++; V s = 0; while(i>0) s += bit[i], i -= i & -i; return s;}
 	BIT(){} BIT(int sz){ n = sz + 1, bit.resize(n,0);} //初期値がすべて0の場合
-	BIT(vector<T>& v){ n = (int)v.size()+1; bit.resize(n, 0); rep(i,n-1) add(i,v[i]);}
+	BIT(vector<V>& v){ n = (int)v.size()+1; bit.resize(n, 0); rep(i,n-1) add(i,v[i]);}
 	void print(){ rep(i,n-1)cout<<sum(i)-sum(i-1)<< " ";cout<<endl;}
 	void print_sum(){ rep(i,n)cout<<sum(i-1)<<" ";cout<<endl;}	//-1スタート
 };
--- competitive_programming/data_structure/BIT_two_dimension.hpp
+++ competitive_programming/data_structure/BIT_two_dimension.hpp
@@ -1,36 +1,39 @@
 #include "../header.hpp"
 
-template<typename T> class BIT {
+template<typename V> class BIT {
 private:
-    int n,m; vector<vector<T> > bit;
+    int n,m; vector<vector<V> > bit;
 public:
-    void add(int x, int y, T val){
-        for(int i = x+1; i < n; i += i&-i){
-            for(int j = y+1; j < m; j += j&-j){
+    void add(int i,int j,V val){
+        i++, j++;
+        while(i < n){
+            while(j < m){
                 bit[i][j] += val;
+                j += j & -j;
             }
+            i += i & -i;
         }
     }
-    T sum(int x, int y){
-        T s = 0;
-        for(int i = x+1; i > 0; i -= i&-i){
-            for(int j = y+1; j > 0; j -= j&-j){
+    V sum(int i,int j){
+        i++, j++;
+        V s = 0;
+        while(i > 0){
+            while(j > 0){
                 s += bit[i][j];
+                j -= j & -j;
             }
+            i -= i & -i;
         }
         return s;
     }
-    T sum(int lx, int ly, int rx, int ry){
-        return sum(rx-1, ry-1) - sum(rx-1, ly-1) - sum(lx-1, ry-1) + sum(lx-1, ly-1);
-    }
     BIT(int sz1,int sz2){
         n = sz1 + 1, m = sz2 + 1;
-        bit.resize(n,vector<T>(m,0));
+        bit.resize(n,vector<V>(m,0));
     }
-    BIT(vector<vector<T> >& v){
+    BIT(vector<vector<V> >& v){
         n = (int)v.size()+1, m = (int)v[0].size()+1;
-        bit.resize(n,vector<T>(m,0));
+        bit.resize(n,vector<V>(m,0));
         rep(i,n) rep(j,m) add(i,j,v[i][j]);
     }
-    void print(){ rep(i,n-1){ rep(j,m-1) cout<<sum(i,j,i+1,j+1)<<" "; cout<<endl;} }
+    void print_sum(){rep(i,n){ rep(j,m)cout<<sum(i-1,j-1)<<" ";cout<<endl;} }
 };
