ae251784841f004d71c111b8dedbaa40ef8e5369
kopricky <kopricky@yahoo.co.jp>
2018-06-30 04:21:59 +0900

    revise

---
+++ competitive_programming/other/ICPC_matrix.hpp
@@ -0,0 +1,77 @@
+#include "../header.hpp"
+
+//乗算がMODの場合はコメントアウトの方を使う
+
+template<typename T> class mat : public vector<vector<T> > {
+public:
+    int r, c;    //行,列
+    mat(int n, int m) : r(n), c(m){
+        for(int i = 0; i < r; i++){
+            this->push_back(vector<T>(c,0));
+        }
+    }
+    mat operator*(const mat& that){
+        mat<T> X(this->r,that.c);
+        for(int i = 0; i < this->r; i++){
+            for(int k = 0; k < this->c; k++){
+                for(int j = 0; j < that.c; j++){
+                    X[i][j] += (*this)[i][k]*that[k][j];
+                }
+            }
+        }
+        return X;
+    }
+    // mat operator*(const mat& that){
+    //     mat<T> X(this->r, that.c);
+    //     for(int i = 0; i < this->r; i++){
+    //         for(int k = 0; k < this->c; k++){
+    //             for(int j = 0; j < that.c; j++){
+    //                 X[i][j] = (X[i][j] + (*this)[i][k]*that[k][j]) % MOD;
+    //             }
+    //         }
+    //     }
+    //     return X;
+    // }
+    void print(){
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c-1; j++){
+                cout << (*this)[i][j] << ",";
+            }
+            cout << (*this)[i][c-1] << endl;
+        }
+    }
+};
+
+template<typename T> mat<T> pow(mat<T> A,ll cnt)
+{
+    int n = A.r;
+	mat<T> B(n,n);
+	for(int i = 0; i < n; i++){
+		B[i][i] = 1;
+	}
+	while(cnt>0){
+		if(cnt & 1){
+			B = B*A;
+		}
+		A = A*A;
+		cnt >>= 1;
+	}
+	return B;
+}
+
+template<typename T> mat<T> mod_pow(mat<T> A,ll cnt)
+{
+    int n = A.r;
+	mat<T> B(n,n);
+	for(int i = 0; i < n; i++){
+		B[i][i] = 1;
+	}
+	while(cnt>0){
+		if(cnt & 1){
+			B = B*A;
+		}
+		A = A*A;
+		cnt >>= 1;
+	}
+	return B;
+}
---
+++ competitive_programming/other/ICPC_rollinghash.hpp
@@ -0,0 +1,46 @@
+#include "../header.hpp"
+
+struct RH {
+	static ll mod[2], mul[2];
+    static vector<ll> pm[2];
+	string s; int sz; vector<ll> hs[2];
+	void init(string& s){
+		this->s = s; sz = (int)s.size();
+        for(int i = 0; i < 2; i++){
+            hs[i].resize(sz+1,0);
+            if(pm[i].empty()) pm[i].push_back(1);
+            for(int j = 0; j < sz; j++){
+                hs[i][j+1] = (hs[i][j]*mul[i]+s[j])%mod[i];
+            }
+        }
+	}
+	pair<ll,ll> hash(int l, int r){	//文字列sのインデックス[l,r)の部分文字列のハッシュ値
+		if(l>=r) return make_pair(0,0);
+        ll res[2];
+        for(int i = 0; i < 2; i++){
+            while((int)pm[i].size() <= r){
+                pm[i].push_back(pm[i].back()*mul[i]%mod[i]);
+            }
+            res[i] = (hs[i][r] + mod[i] - hs[i][l] * pm[i][r-l] % mod[i]) % mod[i];
+        }
+		return make_pair(res[0],res[1]);
+	}
+};
+vector<ll> RH::pm[2];
+ll RH::mod[2]={1000000007, 1000000009}, RH::mul[2]={10009, 10007};
+
+int contain(RH& arg1, string& arg2)
+{
+    int res = 0, sz2 = (int)arg2.size();
+	if(arg1.sz < sz2){
+		return res;
+	}
+	RH rh;
+	rh.init(arg2);
+    for(int i = 0; i < arg1.sz-sz2+1; i++){
+		if(arg1.hash(i,i+sz2) == rh.hash(0,sz2)){
+			res++;
+		}
+	}
+	return res;
+}
