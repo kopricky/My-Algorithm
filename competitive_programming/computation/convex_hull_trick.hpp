f997dd8135b8c44c621d012990306aa821e90da2
kopricky <kopricky@yahoo.co.jp>
2018-06-30 18:11:59 +0900

    revise

--- competitive_programming/computation/convex_hull_trick.hpp
+++ competitive_programming/computation/convex_hull_trick.hpp
@@ -1,55 +1,42 @@
 #include "../header.hpp"
 
-//f[j](x)=a[j]x+b[j]でaがjの増加に伴い単調減少する場合
-//0<=j<=i-kを満たす直線f[j](x)についてminをとる
-//maxをとる場合はcheckの不等号とsolve内のsの移動の部分の不等号を逆にする
+//f[j](x) = a[j]x + b[j]でaがjの増加に伴い単調減少する場合
+//クエリのxが単調でない場合はコメントアウトしたにぶたんの方を用いる
 template<typename T> class CHT
 {
+private:
+    using ptt = pair<T, T>;
+    bool check(ptt l1, ptt l2, ptt l3){
+        return (l2.first-l1.first)*(l3.second-l2.second)>=(l2.second-l1.second)*(l3.first-l2.first);
+    }
+    T f(int i, T x){
+        return lines[i].first * x + lines[i].second;
+    }
 public:
-    //deqには直線のインデックスを格納
-    //dpの初期値はINFづめしている
-    vector<int> deq;
-    vector<T> a,b,dp;
-    int n,k;
-    CHT(vector<T>& A,vector<T>& B,int sz){
-        a = A, b = B;
-        n = (int)a.size(), k = sz;
-        deq.resize(n),dp.resize(n+1,numeric_limits<T>::max()/10);
-    }
-    T func(int j,int x){
-        return a[j]*x+b[j];
-    }
-    bool check(int j1, int j2, int j3){
-        return (a[j2]-a[j1])*(b[j3]-b[j2]) >= (b[j2]-b[j1])*(a[j3]-a[j2]);
-    }
-    //今回の場合は0<=j<=i-kを満たす直線f[j](x)についてminをとっている
-    T solve(){
-        dp[0] = 0, deq[0] = 0;
-        int s = 0, t = 1;
-        //今回の場合はb[i]の値がdp[i]の値に依存するのでb[i]を更新する
-        rep(i,k){
-            b[i] += dp[i];
+    vector<ptt> lines;
+    int head;
+    CHT(): head(0){};
+    void add(T a, T b){
+        ptt line(a, b);
+        while((int)lines.size() - head >= 2 && check(*(lines.end()-2), lines.back(), line)){
+            lines.pop_back();
         }
-        //今回の問題の場合i=k~2k-1まではj=0の場合が最適であることが明らかなので
-        //i-k<kの間はdeqに新たに直線を追加することはしない
-        for(int i=k;i<=n;i++){
-            if(i-k >= k){
-                //末端から最小値を取る可能性がなくなったものを取り除く
-                while(s+1 < t && check(deq[t-2],deq[t-1],i-k)){
-                    t--;
-                }
-                deq[t++] = i-k;
-            }
-            //先頭が最小値でなければ取り除く
-            while(s+1 < t && func(deq[s],i) >= func(deq[s+1],i)){
-                s++;
-            }
-            dp[i] = func(deq[s],i);
-            //今回の場合はb[i]の値がdp[i]の値に依存するのでb[i]を更新する
-            if(i < n){
-                b[i] += dp[i];
-            }
+        lines.emplace_back(line);
+    }
+    T get(T x){
+        while((int)lines.size() - head >= 2 && f(head, x) >= f(head + 1, x)){
+            head++;
         }
-        return dp[n];
+        return f(head, x);
+        // int low = -1, high = lines.size() - 1;
+		// while (high - low > 1) {
+		// 	int mid = (high + low) / 2;
+        //     if(f(mid, x) >= f(mid+1, x)){
+        //         low = mid;
+        //     }else{
+        //         high = mid;
+        //     }
+		// }
+		// return f(high, x);
     }
 };
