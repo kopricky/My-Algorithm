ebb4bbeddc065a82b39a8938d3b159feae8f75e5
kopricky <kopricky@yahoo.co.jp>
2018-07-04 05:31:50 +0900

    revise

---
+++ competitive_programming/data_structure/UnionFind_reverse.hpp
@@ -0,0 +1,44 @@
+#include "../header.hpp"
+//unite操作を巻き戻せるunion find
+//オフラインクエリのグラフの動的連結性判定などに使える
+class UF_pop {
+public:
+    vector<int> par,nrank; vector<vector<int> > hist;
+    int sz;
+    UF_pop(int node_size){
+        sz = node_size;
+        par = nrank = vector<int>(sz,0);
+        rep(i,sz){
+            par[i] = i;
+        }
+    }
+    void reinit(){
+        rep(i,sz){
+            nrank[i] = 0; par[i] = i;
+        }
+    }
+    int find(int x){
+        return (par[x] == x)?(x):find(par[x]);
+    }
+    void pop(){
+        if((int)hist.size()){
+            auto& a = hist.back();
+            par[a[0]] = a[0], nrank[a[0]] = a[1], par[a[2]] = a[2];
+            hist.pop_back();
+        }
+    }
+    void unite(int x,int y) {
+        x = find(x); y = find(y);
+        hist.push_back({x,nrank[x],y});
+        if(x == y) return;
+        if(nrank[x] < nrank[y]){
+            par[x] = y;
+        }else{
+            nrank[x] += (nrank[x] == nrank[y]);
+            par[y] = x;
+        }
+    }
+    bool same(int x,int y){
+        return find(x) == find(y);
+    }
+};
