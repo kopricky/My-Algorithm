9bf092e4ba90db78c7c2d243451dea05db916c17
kopricky <kopricky@yahoo.co.jp>
2018-07-16 13:29:55 +0900

    revise

---
+++ Competitive_Programming/Computation_Basic/Zeller.hpp
@@ -0,0 +1,11 @@
+#include "../header.hpp"
+
+// グレゴリオ暦y年m月d日の曜日(mod7)を返す(Zellerの公式)
+// 土曜日始まりで土曜日が0
+int zeller(int y, int m, int d){
+    if(m <= 2){
+        y--;
+        m += 12;
+    }
+    return (y + y/4 - y/100 + y/400 + (13*m+8)/5 + d) % 7;
+}
