218fdee75f2c01e71ce144f06f65de9d8da23eee
Masataka Gohda <masataka-goda@g.ecc.u-tokyo.ac.jp>
2018-08-03 20:35:32 +0900

    revise

--- Competitive_Programming/String/Aho-Corasick.hpp
+++ Competitive_Programming/String/Aho-Corasick.hpp
@@ -4,30 +4,40 @@ class AhoCorasick
 {
 private:
     using AC = AhoCorasick*;
-    vector<int> ac;
+    vector<pair<int, int> > ac;
     map<int, AC> next;
-    AC fail, dnx;
+    AC t, fail, dnx;
     void add(const string& s, int pos, int id){
         if(pos == (int)s.size()){
-            ac.push_back(id);
+            ac.emplace_back(id, pos);
             return;
         }
         if(!next[s[pos]]) next[s[pos]] = new AhoCorasick();
         next[s[pos]]->add(s,pos+1,id);
     }
-    AC count(vector<int>& res){
+    AC count(vector<vector<int> >& res, int pos){
         if(!fail) return this;
-        for(int id : ac) res[id]++;
-        if(dnx) dnx->count(res);
-        else dnx = fail->count(res);
+        for(auto val : ac) res[val.first].push_back(pos-val.second+1);
+        if(dnx) dnx->count(res,pos);
+        else dnx = fail->count(res,pos);
         return ac.size() ? this : dnx;
+    }
+    void match_(vector<vector<int> >& res, const string& s, int pos=0){
+        if(pos == (int)s.size()) return;
+        if(next[s[pos]]){
+            next[s[pos]]->count(res, pos);
+            next[s[pos]]->match_(res,s,pos+1);
+        }else{
+            if(!fail) match_(res,s,pos+1);
+            else fail->match_(res,s,pos);
+        }
     }
 public:
     AhoCorasick() : fail(nullptr), dnx(nullptr){}
     //パターンを入力として木を構築
-    AC build(vector<string>& pattern){
+    AhoCorasick(vector<string>& pattern): AhoCorasick(){
         int sz = (int)pattern.size();
-        AC t = new AhoCorasick();
+        t = new AhoCorasick();
         rep(i,sz) t->add(pattern[i],0,i);
         queue<AC> que;
         que.push(t);
@@ -45,17 +55,9 @@ public:
                 que.push(nw->next[i]);
             }
         }
-        return t;
     }
-    //sのインデックスpos(普通0)以降に含まれる各パターンの数をresで返す
-    void match(vector<int>& res, const string& s, int pos=0){
-        if(pos == (int)s.size()) return;
-        if(next[s[pos]]){
-            next[s[pos]]->count(res);
-            next[s[pos]]->match(res,s,pos+1);
-        }else{
-            if(!fail) match(res,s,pos+1);
-            else fail->match(res,s,pos);
-        }
+    //sのインデックスpos(デフォルト0)以降に含まれる各パターンの開始位置をresで返す
+    void match(vector<vector<int> >& res, const string& s, int pos=0){
+        t->match_(res,s,pos);
     }
 };
