310bc512b20123baa6d19d8a23e3dde06b6f651a
masataka <kopricky@yahoo.co.jp>
2018-09-12 12:06:25 +0900

    revise

---
+++ Competitive_Programming/SyntaxAnalysis/syntax_analysis_arithmetic.hpp
@@ -0,0 +1,51 @@
+#include "../header.hpp"
+
+// 四則演算の構文解析
+template<typename T> class Parser
+{
+private:
+    T expr(const string& s, int& pos){
+        T res = term(s, pos);
+        while(s[pos] == '+' || s[pos] == '-'){
+            if(s[pos] == '+'){
+                res += term(s, pos++);
+            }else{
+                res -= term(s, pos++);
+            }
+        }
+        return res;
+    }
+    T term(const string& s, int& pos){
+        T res = factor(s, pos);
+        while(s[pos] == '*' || s[pos] == '/'){
+            if(s[pos] == '*'){
+                res *= term(s, pos++);
+            }else{
+                res /= term(s, pos++);
+            }
+        }
+        return res;
+    }
+    T factor(const string& s, int& pos){
+        if(isdigit(s[pos])) return number(s, pos);
+        pos++; // '('をとばす
+        T res = expr(s, pos);
+        pos++; // ')'をとばす
+        return res;
+    }
+    T number(const string& s, int& pos){
+        T res = 0;
+        do{
+            res *= 10;
+            res += (s[pos++] - '0');
+        }while(isdigit(s[pos]));
+        return res;
+    }
+
+public:
+    Parser(){}
+    T solve(const string& s){
+        int pos = 0;
+        return expr(s, pos);
+    }
+};
