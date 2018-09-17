49834d15b97c9fdd6f7ad5b777fd92652b9c2fc5
masataka <kopricky@yahoo.co.jp>
2018-09-18 06:56:23 +0900

    revise

--- Competitive_Programming/String/manacher.hpp
+++ Competitive_Programming/String/manacher.hpp
@@ -4,19 +4,19 @@
 
 //iを中心とする最長の回文の半径をR[i]に格納(O(n))
 // abaab を $a$b$a$a$b$ みたいにすると偶数長のもの求めることが可能
-int R[MAX_N];
-
-void manacher(string& S)
+void manacher(string& S,vector<int>& res)
 {
+    int sz = (int)S.size();
+    res.resize(sz);
     int np=0,nr=0;
-    while (np < (int)S.size()) {
-        while(np-nr >= 0 && np+nr < (int)S.size() && S[np-nr] == S[np+nr]){
+    while(np < sz){
+        while(np-nr >= 0 && np+nr < sz && S[np-nr] == S[np+nr]){
             nr++;
         }
-        R[np] = nr;
+        res[np] = nr;
         int k = 1;
-        while(np-k >= 0 && np+k < (int)S.size() && k+R[np-k] < nr){
-            R[np+k] = R[np-k];
+        while(np-k >= 0 && np+k < sz && k+res[np-k] < nr){
+            res[np+k] = res[np-k];
             k++;
         }
         np += k; nr -= k;
