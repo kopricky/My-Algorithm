9876d9fdb0982df93f102acc036401f697e3c9c0
kopricky <kopricky@yahoo.co.jp>
2018-10-07 20:46:24 +0900

    revise

--- Competitive_Programming/NetworkFlow/Dinic.hpp
+++ Competitive_Programming/NetworkFlow/Dinic.hpp
@@ -3,7 +3,7 @@
 //最大流量がint,頂点数がMAX_N
 //Dinic<int> dn(MAX_N);
 //適宜add_edge,max_flowを用いる
-template<typename T> class Dinic {
+template&lt;typename T&gt; class Dinic {
 private:
     struct edge{
         int to;
@@ -11,34 +11,32 @@ private:
         int rev;
     };
     int V;
-    vector<vector<edge> > G;
-    vector<int> level,iter;
-    void bfs(int s)
-    {
+    vector&lt;vector&lt;edge&gt; &gt; G;
+    vector&lt;int&gt; level,iter;
+    void bfs(int s) {
     	fill(level.begin(),level.end(),-1);
-    	queue<int> que;
+    	queue&lt;int&gt; que;
     	level[s] = 0;
     	que.push(s);
     	while(!que.empty()){
     		int v = que.front();
     		que.pop();
-    		for(auto& e : G[v]){
-    			if(e.cap > 0 && level[e.to] < 0){
+    		for(auto&amp; e : G[v]){
+    			if(e.cap &gt; 0 &amp;&amp; level[e.to] &lt; 0){
     				level[e.to] = level[v] + 1;
     				que.push(e.to);
     			}
     		}
     	}
     }
-    T dfs(int v,int t,T f)
-    {
+    T dfs(int v,int t,T f) {
     	if(v==t){
     		return f;
     	}
-        for(edge& e : G[v]){
-    		if(e.cap > 0 && level[v] < level[e.to]){
+        for(edge&amp; e : G[v]){
+    		if(e.cap &gt; 0 &amp;&amp; level[v] &lt; level[e.to]){
     			T d = dfs(e.to,t,min(f,e.cap));
-    			if(d>0){
+    			if(d&gt;0){
     				e.cap -= d;
     				G[e.to][e.rev].cap += d;
     				return d;
@@ -47,26 +45,25 @@ private:
     	}
     	return 0;
     }
+
 public:
     Dinic(int node_size) : V(node_size), G(V), level(V), iter(V){}
     //辺を張る
-    void add_edge(int from,int to,T cap)
-    {
+    void add_edge(int from,int to,T cap) {
     	G[from].push_back((edge){to,cap,(int)G[to].size()});
     	G[to].push_back((edge){from,(T)0,(int)G[from].size()-1});
     }
     //最大流を計算
-    T solve(int s,int t)
-    {
+    T solve(int s,int t) {
     	T flow = 0;
     	for(;;){
     		bfs(s);
-    		if(level[t]<0){
+    		if(level[t]&lt;0){
     			return flow;
     		}
     		fill(iter.begin(),iter.end(),0);
     		T f;
-    		while((f=dfs(s,t,numeric_limits<T>::max())) > 0){
+    		while((f=dfs(s,t,numeric_limits&lt;T&gt;::max())) &gt; 0){
     			flow += f;
     		}
     	}
