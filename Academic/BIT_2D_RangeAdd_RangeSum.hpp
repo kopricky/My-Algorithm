498d95d5e0ce31e9bf46e11fc60700d642218d6b
kopricky <kopricky@yahoo.co.jp>
2018-10-30 00:06:34 +0900

    revise

--- Academic/BIT_2D_RangeAdd_RangeSum.hpp
+++ Academic/BIT_2D_RangeAdd_RangeSum.hpp
@@ -8,51 +8,41 @@
 //具体的にはd次元について計算量はO(4^d*log^d(n))
 //Mishra 2013 を参照
 
-template<typename V> class BIT {
+template<typename T> class BIT {
 private:
-    int n,m; vector<vector<V> > bit;
+    int n,m; vector<vector<T> > bit;
 public:
-    void add(int i,int j,V val){
-        i++, j++;
-        while(i < n){
-            while(j < m){
-                bit[i][j] += val;
-                j += j & -j;
-            }
-            i += i & -i;
-        }
+    void add(int i, int j, T val){
+        for(int i_ = i+1; i_ < n; i_ += i_ & -i_)
+            for(int j_ = j+1; j_ < m; j_ += j_ & -j_)
+                bit[i_][j_] += val;
     }
-    V sum(int i,int j){
-        i++, j++;
-        V s = 0;
-        while(i > 0){
-            while(j > 0){
-                s += bit[i][j];
-                j -= j & -j;
-            }
-            i -= i & -i;
-        }
-        return s;
+    T sum(int i, int j){
+        T s = 0;
+            for(int i_ = i+1; i_ > 0; i_ -= i_ & -i_)
+                for(int j_ = j+1; j_ > 0; j_ -= j_ & -j_)
+                    s += bit[i_][j_];
+            return s;
     }
-    BIT(int sz1,int sz2){
+    T sum(int lx, int uy, int rx, int dy){
+        return sum(rx, dy) - sum(lx-1, dy) - sum(rx, uy-1) + sum(lx-1, uy-1);   
+    }
+    BIT(int sz1, int sz2){
         n = sz1 + 1, m = sz2 + 1;
-        bit.resize(n,vector<V>(m,0));
+        bit.resize(n, vector<T>(m, 0));
     }
-    BIT(vector<vector<V> >& v){
+    BIT(vector<vector<T> >& v){
         n = (int)v.size()+1, m = (int)v[0].size()+1;
-        bit.resize(n,vector<V>(m,0));
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                add(i,j,v[i][j]);
-            }
-        }
+        bit.resize(n, vector<T>(m, 0));
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                add(i, j, v[i][j]);
     }
     void print_sum(){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                cout<<sum(i-1,j-1)<<" ";cout<<endl;
-            }
-        }
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                cout<< sum(i-1, j-1) << " ";
+        cout << "\n";
     }
 };
 
