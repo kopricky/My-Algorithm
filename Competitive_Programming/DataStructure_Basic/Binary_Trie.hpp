6a3e405ac8edbc1f9673978ae367eaf581bc735e
kopricky <kopricky@yahoo.co.jp>
2018-11-02 13:54:08 +0900

    revise

--- Competitive_Programming/DataStructure_Basic/Binary_Trie.hpp
+++ Competitive_Programming/DataStructure_Basic/Binary_Trie.hpp
@@ -11,33 +11,33 @@ public:
 	};
 	Node* root;
 	//2^30より小さい場合
-    const int MAX_BIT = 30;
+	const int MAX_BIT = 30;
 	BinaryTrie(){
 		root = new Node();
 	}
 	//Trie木にxを加える
 	void add(int x) {
 		Node *curr = root;
-        for(int i = MAX_BIT-1; i >= 0; i--){
-            int y = x >> i & 1;
+		for(int i = MAX_BIT-1; i >= 0; i--){
+			int y = x >> i & 1;
 			if (!curr->next[y]) {
 				curr->next[y] = new Node();
 			}
 			curr->sub++;
 			curr = curr->next[y];
-        }
+		}
 		curr->sub++;
 	}
-    //何らかのクエリ
+	//何らかのクエリ
 	int query(Node *curr,const int a,int d){
 		if(!curr) return 0;
 		if((a >> d) & 1){
-            return query(curr->next[1],a,d-1);
+			return query(curr->next[1],a,d-1);
 		}else{
-            return query(curr->next[0],a,d-1);
-        }
+			return query(curr->next[0],a,d-1);
+		}
 	}
-    int query(const int a){
-        return query(root,a,MAX_BIT-1);
-    }
+	int query(const int a){
+		return query(root,a,MAX_BIT-1);
+	}
 };
