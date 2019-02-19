6f7c4325ce3010a74efd4f87175a3162247b2961
kopricky <kopricky@yahoo.co.jp>
2019-02-19 22:00:36 +0900

    revise

--- Competitive_Programming/DataStructure_Basic/UpdateInterval_plus.hpp
+++ Competitive_Programming/DataStructure_Basic/UpdateInterval_plus.hpp
@@ -1,13 +1,13 @@
 #include "../header.hpp"
 
 // セグ木で持てないくらい大きい範囲の区間更新のクエリに答える
-// mpは(key=区間の左端:val=値)で初めに(-inf,-1)を詰めておく
+// mpは(key=区間の左端:val=値)でコンストラクタで(-inf,0)を詰めている
 template<typename CandType, typename ValType> class UpdateInterval
 {
 public:
     map<CandType, ValType> interval;
     UpdateInterval(){
-        interval[numeric_limits<CandType>::min()] = -1;
+        interval[numeric_limits<CandType>::min()] = 0;
     }
     // [l,r) の区間を値 val に更新する
     void update(CandType l, CandType r, ValType val) {
