8c5bb1ffb42a35550a8b70fc9350eb360f296e0b
kopricky <kopricky@yahoo.co.jp>
2019-02-23 16:24:46 +0900

    revise

---
+++ Tests/test.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INF 1000000005
+#define MOD 1000000007
+#define EPS 1e-10
+#define rep(i,n) for(int i=0;i<(int)(n);++i)
+#define rrep(i,n) for(int i=(int)(n)-1;i>=0;--i)
+#define srep(i,s,t) for(int i=(int)(s);i<(int)(t);++i)
+#define each(a,b) for(auto& (a): (b))
+#define all(v) (v).begin(),(v).end()
+#define len(v) (int)(v).size()
+#define zip(v) sort(all(v)),v.erase(unique(all(v)),v.end())
+#define cmx(x,y) x=max(x,y)
+#define cmn(x,y) x=min(x,y)
+#define fi first
+#define se second
+#define pb push_back
+#define show(x) cout<<#x<<" = "<<(x)<<endl
+
+using namespace std;
+
+template<typename S,typename T>auto&operator<<(ostream&o,pair<S,T>&p){return o<<"{"<<p.fi<<","<<p.se<<"}";}
+template<typename T>auto&operator<<(ostream&o,set<T>&s){for(auto&e:s)o<<e<<" ";return o;}
+template<typename S,typename T,typename U>
+auto&operator<<(ostream&o,priority_queue<S,T,U>q){while(!q.empty())o<<q.top()<<" ",q.pop();return o;}
+template<typename K,typename T>auto&operator<<(ostream&o,map<K,T>&m){for(auto&e:m)o<<e<<" ";return o;}
+template<typename T>auto&operator<<(ostream&o,vector<T>&v){for(auto&e:v)o<<e<<" ";return o;}
+template<typename...A>void ashow(A&...a){for(auto&e:{a...})cout<<e<<" ";cout<<endl;}
+
+typedef pair<int,int> P;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<P> vp;
+typedef vector<double> vd;
+typedef vector<string> vs;
+
+const int MAX_N = 100005;
+
+ll gcd(ll a,ll b)
+{
+    return b ? gcd(b, a % b) : a;
+}
+
+ll lcm(ll a,ll b)
+{
+    return  a / gcd(a,b) * b;
+}
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    ll a,b,c;
+    cin >> a >> b >> c;
+    cout << min({a,b,c}) << " " << max({a,b,c}) << "\n";
+    return 0;
+}
--- Tests/test.py
+++ Tests/test.py
@@ -3,26 +3,38 @@ import urllib.parse
 import urllib.request
 import urllib.error
 import json
-import requests
+import sys
+
 
 def main():
-    problem_url = input()
+    args = sys.argv
+    code_path, problem_url = args[1], args[2]
+    abs_path = '/home/meip-users/my_algorithm/Tests'
+    exec_check = subprocess.call('g++ -std=c++14 -o {}/exec ./{}'.format(abs_path, code_path).split())
+    if exec_check == 1:
+        print('コンパイル失敗')
+        return
+    else:
+        print('コンパイル成功')
     parsed_url = urllib.parse.urlparse(problem_url)
-    problem_number = parsed_url.query[3:]
-    url = 'https://judgedat.u-aizu.ac.jp/testcases/{}/header'.format(problem_number)
-    req = urllib.request.Request(url)
-    try:
-        with urllib.request.urlopen(req) as res:
-            body = json.load(urllib.request.urlopen(req))
-    except urllib.error.HTTPError as err:
-        print(err.code)
-    except urllib.error.URLError as err:
-        print(err.reason)
+    problem_id = parsed_url.query[3:]
+    url = 'https://judgedat.u-aizu.ac.jp/testcases/{}/header'.format(problem_id)
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
+    req = urllib.request.Request(url, method='GET', headers={'User-Agent':user_agent})
+    body = json.load(urllib.request.urlopen(req))
     for header in body['headers']:
         for type in ['in', 'out']:
-            testcase_url = 'https://judgedat.u-aizu.ac.jp/testcases/{}/{}/{}'.format(problem_number, header['serial'], type)
-            subprocess.call('wget {} -O ./test_case/{}.{}'.format(testcase_url, header['serial'], type).split())
-        if subprocess.check_output()
+            testcase_url = 'https://judgedat.u-aizu.ac.jp/testcases/{}/{}/{}'.format(problem_id, header['serial'], type)
+            subprocess.call('wget {} -q -O {}/test_case/{}.{}'.format(testcase_url, abs_path, header['serial'], type).split())
+        input_file = open('{}/test_case/{}.in'.format(abs_path, header['serial']), mode='r')
+        output_file = open('{}/test_case/{}.txt'.format(abs_path, header['serial']), mode='w')
+        subprocess.run('{}/exec'.format(abs_path), stdin=input_file, stdout=output_file, shell=True)
+        dif_res = subprocess.check_output('diff {}/test_case/{}.txt {}/test_case/{}.out'.format(abs_path, header['serial'], abs_path, header['serial']).split())
+        if dif_res == b'':
+            print('Testcase {}: AC'.format(header['serial']))
+        else:
+            print('Testcase {}: WA'.format(header['serial']))
+            break
 
 if __name__ == '__main__':
     main()
