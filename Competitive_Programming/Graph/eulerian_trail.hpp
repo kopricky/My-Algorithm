3944e95f2de7d9e2318ab66148a8af9caf54c836
masataka <kopricky@yahoo.co.jp>
2019-04-25 01:35:29 +0200

    revise

--- Competitive_Programming/Graph/eulerian_trail.hpp
+++ Competitive_Programming/Graph/eulerian_trail.hpp
@@ -1,69 +1,101 @@
 #include "../header.hpp"
 
-//無向グラフ：奇次数の頂点が0or2個
-//有向グラフ：全ての頂点の相対次数が0or相対次数が1,-1の頂点が1つずつ
-//Hierholzer's Algorithm
-//アルゴリズムは線形だが実装にmapを用いているためlogが余計にかかっている
+// グラフが連結であることを仮定
+// 無向グラフ：奇次数の頂点が0or2個
+// 有向グラフ：全ての頂点の相対次数が0or相対次数が1,-1の頂点が1つずつ
+// Hierholzer's Algorithm
 
+// 無向の場合
+class EulerPath
+{
+private:
+    struct edge {
+        int to;
+        list<edge>::const_iterator rev;
+        edge(int to_) : to(to_){}
+    };
+public:
+    int V;
+    list<edge>* G;
+    vector<int> ans;
+    EulerPath(int node_size) : V(node_size){ G = new list<edge>[V]; }
+    void add_edge(int u, int v){
+        G[u].push_back((edge){v}); list<edge>::const_iterator revu = (--G[u].end());
+        G[v].push_back((edge){u}); list<edge>::const_iterator revv = (--G[v].end());
+        G[u].back().rev = revv, G[v].back().rev = revu;
+    }
+    int judge(){
+        int s = -1, odd = 0;
+        for(int i = 0; i < V; i++){
+            if((int)G[i].size() % 2){
+                if(++odd >= 3) return -1;
+                s = i;
+            }
+        }
+        return max(s, 0);
+    }
+    bool solve(){
+        int cur = judge();
+        if(V == 0 || cur < 0) return false;
+        stack<int> cur_path;
+        cur_path.push(cur);
+        while(!cur_path.empty()){
+            if(!G[cur].empty()){
+                cur_path.push(cur);
+                auto nx = G[cur].back();
+                G[cur].pop_back(), G[nx.to].erase(nx.rev);
+                cur = nx.to;
+            }else{
+                ans.push_back(cur);
+                cur = cur_path.top(), cur_path.pop();
+            }
+        }
+        return true;
+    }
+};
+
+// 有向の場合
 class EulerPath
 {
 public:
-    int V; bool directed;
-    vector<map<int,int> > G;
-    vector<int> degree,path;
-    EulerPath(int node_size, bool isdirected=false) :
-            V(node_size), directed(isdirected), G(V), degree(V, 0){}
-    void add_edge(int u,int v){
-        if(directed){
-            G[u][v]++;
-            degree[u]++,degree[v]--;
-        }else{
-            G[u][v]++,G[v][u]++;
-            degree[u]++,degree[v]++;
-        }
+    int V;
+    vector<vector<int> > G;
+    vector<int> degree, ans;
+    EulerPath(int node_size) : V(node_size), G(V), degree(V, 0){}
+    void add_edge(int u, int v){
+        G[u].push_back(v), degree[u]++, degree[v]--;
     }
-    void dfs(int u){
-        while(!G[u].empty()){
-            auto it = G[u].begin();
-            int p = (*it).first,q = (*it).second;
-            if(q > 0){
-                if(directed){
-                    G[u][p]--;
-                }else{
-                    G[u][p]--,G[p][u]--;
-                }
-                dfs(p);
-            }else{
-                it = G[u].erase(it);
-            }
+    int judge(){
+        int s = -1; bool out = false, in = false;
+        for(int i = 0; i < V; i++){
+            if(degree[i] == 0) continue;
+            if(degree[i] == 1){
+                if(out) return -1;
+                out = true, s = i;
+            }else if(degree[i] == -1){
+                if(in) return -1;
+                in = true;
+            }else return -1;
         }
-        path.push_back(u);
+        return max(s, 0);
     }
     bool solve(){
-        int s = -1, t = -1;
-        if(directed){
-            for(int i = 0; i < V; i++){
-                if(degree[i] != 0){
-                    if(degree[i] == 1){
-                        if(s >= 0) return false;
-                        s = i;
-                    }else if(degree[i] == -1){
-                        if(t >= 0) return false;
-                        t = i;
-                    }else return false;
-                }
-            }
-        }else{
-            for(int i = 0; i < V; i++){
-                if(degree[i] % 2){
-                    if(s < 0) s = i;
-                    else if(t < 0) t = i;
-                    else return false;
-                }
+        int cur = judge();
+        if(V == 0 || cur < 0) return false;
+        stack<int> cur_path;
+        cur_path.push(cur);
+        while(!cur_path.empty()){
+            if(!G[cur].empty()){
+                cur_path.push(cur);
+                int nx = G[cur].back();
+                G[cur].pop_back();
+                cur = nx;
+            }else{
+                ans.push_back(cur);
+                cur = cur_path.top(), cur_path.pop();
             }
         }
-        if(s < 0) s = 0;
-        dfs(s);
+        reverse(ans.begin(), ans.end());
         return true;
     }
 };
--- Competitive_Programming/OriginalContainer/list.hpp
+++ Competitive_Programming/OriginalContainer/list.hpp
@@ -7,28 +7,36 @@ template<typename T>
 class List {
 public:
     using iterator = ListIterator<T>;
-    
+
 private:
+    struct block {
+        int prev, next;
+        T value;
+        block(){}
+        block(T value_) : value(value_){}
+        block(int prev_, int next_) : prev(prev_), next(next_){}
+    };
     int N, sz;
-    vector<int> prev, next;
-    vector<T> container;
+    vector<block> container;
     friend ListIterator<T>;
-    
+
 public:
     // 添字 next_index の要素の前に添字 now_index の要素を挿入
     iterator insert(int next_index, int now_index){
         sz++;
-        prev[now_index] = prev[next_index], next[now_index] = next_index;
-        next[prev[next_index]] = now_index, prev[next_index] = now_index;
+        auto& nw = container[now_index]; auto& nx = container[next_index];
+        nw.prev = nx.prev, nw.next = next_index;
+        container[nx.prev].next = now_index, nx.prev = now_index;
         return iterator(this, now_index);
     }
     // 添字 index の値を削除 → 次の要素の iterator を返す
     iterator erase(int index){
         sz--;
-        next[prev[index]] = next[index], prev[next[index]] = prev[index];
-        return iterator(this, next[index]);
+        auto& cur = container[index];
+        container[cur.prev].next = cur.next, container[cur.next].prev = cur.prev;
+        return iterator(this, cur.next);
     }
-	
+
 public:
     friend ostream& operator<< (ostream& os, List& ls){
         for(auto& val : ls){
@@ -37,10 +45,10 @@ public:
         return os;
     }
     const T& operator[](size_t index) const {
-        return container[index];
+        return container[index].value;
     }
     T& operator[](size_t index){
-        return container[index];
+        return container[index].value;
     }
     size_t size(){
         return sz;
@@ -49,13 +57,13 @@ public:
         return size() == 0;
     }
     T& front(){
-        return container[next[N]];
+        return container[container[N].next].value;
     }
     T& back(){
-        return container[prev[N]];
+        return container[container[N].prev].value;
     }
     iterator begin(){
-        return iterator(this, next[N]);
+        return iterator(this, container[N].next);
     }
     iterator end(){
         return iterator(this, N);
@@ -67,39 +75,27 @@ public:
         return erase(itr.index);
     }
     iterator push_back(){
-        return insert(N, prev[N]+1);
+        return insert(N, container[N].prev+1);
     }
     void pop_back(){
-        erase(prev[N]);
+        erase(container[N].prev);
     }
     void index_clear(){
-        N = sz = 0, prev = next = {0};
+        N = sz = 0, container = {(block){0, 0}};
     }
     void clear(){
-        index_clear();
-        container.clear();
+        index_clear(); container.clear();
     }
-	
+
 public:
-    List() : N(0), sz(0), prev({0}), next({0}){}
-    List(int _N) : container(_N){
-        build();
-    }
-    List(int _N, T val) : container(_N, val){
+    List() : N(0), sz(0), container({(block){0, 0}}){}
+    List(int _N) : container(_N + 1){
         build();
     }
     List(const List& ls):
-        N(ls.N), sz(ls.sz), prev(ls.prev), next(ls.next), container(ls.container){}
-    List(const vector<T>& vec) : container(vec){
-        build();
-    }
+        N(ls.N), sz(ls.sz), container(ls.container){}
     List& operator=(const List& ls){
         container = ls.container;
-        build();
-        return *this;
-    }
-    List& operator=(const vector<T>& vec){
-        container = vec;
         build();
         return *this;
     }
@@ -108,21 +104,16 @@ public:
         build();
         return *this;
     }
-    List(initializer_list<T> vals) : container(vals.begin(), vals.end()){
-        build();
-    }
     iterator push_back(T val){
         N++, sz++;
-        prev.push_back(N-1), prev[0] = N;
-        next.back() = N, next.push_back(0);
-        container.push_back(val);
+        container[0].prev = container.back().next = N, container.back().value = val;
+        container.emplace_back(N-1, 0);
         return iterator(this, N-1);
     }
 	void build(){
-        N = sz = (int)container.size();
-        prev.resize(N+1), next.resize(N+1);
-        iota(prev.begin(), prev.end(), -1), iota(next.begin(), next.end(), 1);
-        prev[0] = N, next[N] = 0;
+        N = sz = (int)container.size() - 1;
+        for(int i = 0; i <= N; i++) container[i].prev = i-1, container[i].next = i+1;
+        container[0].prev = N, container[N].next = 0;
     }
 };
 
@@ -135,7 +126,7 @@ private:
 
 private:
     ListIterator(List<T>* ls, int _index) : ls_ptr(ls), index(_index){}
-	
+
 public:
     ListIterator(){}
     ListIterator(const ListIterator& itr){
@@ -151,27 +142,27 @@ public:
         return *this;
     }
     T& operator*() const {
-        return ls_ptr->container[index];
+        return ls_ptr->container[index].value;
     }
     T* operator->() const {
-        return &ls_ptr->container[index];
+        return &ls_ptr->container[index].value;
     }
     ListIterator& operator++(){
-        index = ls_ptr->next[index];
+        index = ls_ptr->container[index].next;
         return *this;
     }
     ListIterator operator++(int){
         ListIterator res = *this;
-        index = ls_ptr->next[index];
+        index = ls_ptr->container[index].next;
         return res;
     }
     ListIterator& operator--(){
-        index = ls_ptr->prev[index];
+        index = ls_ptr->container[index].prev;
         return *this;
     };
     ListIterator operator--(int){
         ListIterator res = *this;
-        index = ls_ptr->prev[index];
+        index = ls_ptr->container[index].prev;
         return res;
     };
     bool operator==(const ListIterator& itr) const {
