ec2a5e9a9a2844aed1a360368b39fe0ed06bdac6
masataka <kopricky@yahoo.co.jp>
2019-07-08 23:46:12 +0200

    revise

--- Competitive_Programming/ParallelProgramming/lock_free_stack.hpp
+++ Competitive_Programming/ParallelProgramming/lock_free_stack.hpp
@@ -1,31 +1,77 @@
 #include "parallel_header.hpp"
 
-// pop 時の exception を handle するために T data を持って shared_ptr<T> を pop 時に返すこともできるが, shared_ptr<T> の
-// 参照先であるデータ(node) を push 時に new などで heap 領域に確保する必要があるが、 結局領域を確保する際に
-// exception を吐く可能性があるのであまり良くない
-
 template<typename T>
 class LockFreeStack
 {
 private:
+    struct node;
+    struct counted_node_ptr
+    {
+        int external_count;
+        node* ptr;
+    };
     struct node
     {
         std::shared_ptr<T> data;
-        node* next;
-        node(const T& data_) : data(std::make_shared<T>(data_)){}
+        std::atomic<int> internal_count;
+        counted_node_ptr next;
+        node(const T& data_) :
+            data(std::make_shared<T>(data_)), internal_count(0){}
     };
-    std::atomic<node*> head;
+    std::atomic<counted_node_ptr> head;
+    void increase_head_count(counted_node_ptr& old_counter)
+    {
+        counted_node_ptr new_counter;
+        do
+        {
+            new_counter = old_counter;
+            ++new_counter.external_count;
+        }
+        while(!head.compare_exchange_strong(old_counter, new_counter,
+                                            std::memory_order_acquire, std::memory_order_relaxed));
+    }
 public:
+    ~LockFreeStack()
+    {
+        while(pop());
+    }
     void push(const T& data)
     {
-        node* const new_node = new node(data);
-        new_node->next = head.load();
-        while(!head.compare_exchange_weak(new_node->next, new_node));
+        counted_node_ptr new_node;
+        new_node.ptr = new node(data);
+        new_node.external_count = 1;
+        new_node.ptr->next = head.load(std::memory_order_relaxed);
+        while(!head.compare_exchange_weak(new_node.ptr->next, new_node,
+                                            std::memory_order_release, std::memory_order_relaxed));
     }
     std::shared_ptr<T> pop()
     {
-        node* old_head = head.load();
-        while(old_head && !head.compare_exchange_weak(old_head, old_head->next));
-        return old_head ? old_head->data : std::shared_ptr<T>();
+        counted_node_ptr old_head = head.load(std::memory_order_relaxed);
+        for(;;)
+        {
+            increase_head_count(old_head);
+            node* const ptr = old_head.ptr;
+            if(!ptr)
+            {
+                return std::shared_ptr<T>();
+            }
+            if(head.compare_exchange_strong(old_head, ptr->next, std::memory_order_relaxed))
+            {
+                std::shared_ptr<T> res;
+                res.swap(ptr->data);
+                int const count_increase = old_head.external_count - 2;
+                if(ptr->internal_count.fetch_add(count_increase,
+                        std::memory_order_release) == -count_increase)
+                {
+                    delete ptr;
+                }
+                return res;
+            }
+            else if(ptr->internal_count.fetch_add(-1, std::memory_order_relaxed) == 1)
+            {
+                ptr->internal_count.load(std::memory_order_acquire);
+                delete ptr;
+            }
+        }
     }
 };
