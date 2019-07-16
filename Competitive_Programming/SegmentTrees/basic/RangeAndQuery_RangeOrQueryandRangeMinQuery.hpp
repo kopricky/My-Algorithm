be2b27233189ec3a18aa6277f1defc82b5d4f873
masataka <kopricky@yahoo.co.jp>
2019-07-16 05:50:34 +0200

    revise

--- Competitive_Programming/SegmentTrees/basic/RangeAndQuery_RangeOrQueryandRangeMinQuery.hpp
+++ Competitive_Programming/SegmentTrees/basic/RangeAndQuery_RangeOrQueryandRangeMinQuery.hpp
@@ -9,15 +9,15 @@ template<typename T> class segtree
 {
 private:
     int n,sz;
-    vector<T> node, And, Or, lazy;
+    vector<T> node, _and, _or, lazy;
     void update(int id){
         node[id] = min(node[2*id+1],node[2*id+2]);
-        And[id] = (And[2*id+1] & And[2*id+2]);
-        Or[id] = (Or[2*id+1] | Or[2*id+2]);
+        _and[id] = (_and[2*id+1] & _and[2*id+2]);
+        _or[id] = (_or[2*id+1] | _or[2*id+2]);
     }
     void eval(int k, int l, int r){
         if(lazy[k] != 0){
-            node[k] += lazy[k], And[k] += lazy[k], Or[k] += lazy[k];
+            node[k] += lazy[k], _and[k] += lazy[k], _or[k] += lazy[k];
             if(r - l > 1) {
                 lazy[2*k+1] += lazy[k], lazy[2*k+2] += lazy[k];
             }
@@ -27,10 +27,10 @@ private:
 public:
     segtree(const vector<T>& v) : n(1), sz((int)v.size()){
         while(n < sz) n *= 2;
-        node.resize(2*n-1, numeric_limits<T>::max()), And.resize(2*n-1, 0);
-        Or.resize(2*n-1, 0), lazy.resize(2*n-1, 0);
+        node.resize(2*n-1, numeric_limits<T>::max()), _and.resize(2*n-1, 0);
+        _or.resize(2*n-1, 0), lazy.resize(2*n-1, 0);
         for(int i = 0; i < sz; i++){
-            node[i+n-1] = And[i+n-1] = Or[i+n-1] = v[i];
+            node[i+n-1] = _and[i+n-1] = _or[i+n-1] = v[i];
         }
         for(int i=n-2; i>=0; i--){
             update(i);
@@ -39,11 +39,11 @@ public:
     void range_and(int a, int b, T x, int k=0, int l=0, int r=-1){
         if(r < 0) r = n;
         eval(k, l, r);
-        if(b <= l || r <= a || !(~x & Or[k])){
+        if(b <= l || r <= a || !(~x & _or[k])){
             return;
         }
-        if(a <= l && r <= b && (~x & And[k]) == (~x & Or[k])){
-            lazy[k] -= (~x & And[k]);
+        if(a <= l && r <= b && (~x & _and[k]) == (~x & _or[k])){
+            lazy[k] -= (~x & _and[k]);
             eval(k,l,r);
         }else{
             range_and(a, b, x, 2*k+1, l, (l+r)/2), range_and(a, b, x, 2*k+2, (l+r)/2, r);
@@ -53,11 +53,11 @@ public:
     void range_or(int a, int b, T x, int k=0, int l=0, int r=-1){
         if(r < 0) r = n;
         eval(k, l, r);
-        if(b <= l || r <= a || !(x & ~And[k])){
+        if(b <= l || r <= a || !(x & ~_and[k])){
             return;
         }
-        if(a <= l && r <= b && (x & ~Or[k]) == (x & ~And[k])){
-            lazy[k] += (x & ~Or[k]);
+        if(a <= l && r <= b && (x & ~_or[k]) == (x & ~_and[k])){
+            lazy[k] += (x & ~_or[k]);
             eval(k,l,r);
         }else{
             range_or(a, b, x, 2*k+1, l, (l+r)/2), range_or(a, b, x, 2*k+2, (l+r)/2, r);
