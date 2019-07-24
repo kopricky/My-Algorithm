57b6a8441d8d2c9a8f281000ef30ed3a3ce92025
masataka <kopricky@yahoo.co.jp>
2019-07-25 01:22:10 +0200

    revise

--- Academic/max_flow_PushRelabel.hpp
+++ Academic/max_flow_PushRelabel.hpp
@@ -5,14 +5,14 @@
 //O(n^2 m^(1/2)) Goldberg and Tarjan 1988 を参照(動的木により計算量は改善可能)
 class Stack {
 private:
-    int N, H;
+    const int N, H;
     vector<int> node;
 public:
-    Stack(int _N, int _H) : N(_N), H(_H), node(N+H){ clear(); }
-    inline bool empty(int h){ return node[N+h] == N+h; }
-    inline int top(int h){ return node[N+h]; }
-    inline void pop(int h){ node[N+h] = node[node[N+h]]; }
-    inline void push(int h, int u){ node[u] = node[N+h], node[N+h] = u; }
+    Stack(const int _N, const int _H) : N(_N), H(_H), node(N+H){ clear(); }
+    inline bool empty(const int h){ return node[N+h] == N+h; }
+    inline int top(const int h){ return node[N+h]; }
+    inline void pop(const int h){ node[N+h] = node[node[N+h]]; }
+    inline void push(const int h, const int u){ node[u] = node[N+h], node[N+h] = u; }
     inline void clear(){ iota(node.begin() + N, node.end(), N); }
 };
 
@@ -22,17 +22,18 @@ public:
     struct node {
         int prev, next;
     };
-    int N, H;
+    const int N, H;
     vector<size_t> sz;
     vector<node> dat;
-    List(int _N, int _H) : N(_N), H(_H), sz(H, 0), dat(N+H){ clear(); }
-    inline size_t size(int h) const { return sz[h]; }
-    inline void insert(int h, int u){
+    List(const int _N, const int _H) : N(_N), H(_H), sz(H, 0), dat(N+H){ clear(); }
+    inline size_t size(const int h) const { return sz[h]; }
+    inline bool empty(const int h) const { return (sz[h] == 0); }
+    inline void insert(const int h, const int u){
         ++sz[h];
         dat[u].prev = dat[N+h].prev, dat[u].next = N+h;
         dat[dat[N+h].prev].next = u, dat[N+h].prev = u;
     }
-    inline void erase(int h, int u){
+    inline void erase(const int h, const int u){
         --sz[h];
         dat[dat[u].prev].next = dat[u].next, dat[dat[u].next].prev = dat[u].prev;
     }
@@ -50,65 +51,59 @@ public:
         edge(int _to, int _rev, T _cap) : to(_to), rev(_rev), cap(_cap){}
     };
 private:
-    int V, s, t, pot_max, checker;
+    const int V;
+    int s, t, pot_max, checker;
     vector<T> excess;
     vector<int> potential, cur_edge, que;
     List all_ver;
     Stack act_ver;
-    int init(){
+    int calc_active(){
+        pot_max = -1;
+        for(int i = 0; i < V; ++i){
+            if(potential[i] < V){
+                cur_edge[i] = 0;
+                pot_max = max(potential[i], pot_max);
+                all_ver.insert(potential[i], i);
+                if(excess[i] > 0 && i != t) act_ver.push(potential[i], i);
+            }else{
+                potential[i] = V+1;
+            }
+        }
+        return pot_max;
+    }
+    void bfs(){
+        for(int i = 0; i < V; ++i) potential[i] = max(potential[i], V);
         potential[t] = 0;
         int qh = 0, qt = 0;
-        for(que[qt++] = t; qh < qt;){
-            int u = que[qh++], h = potential[u] + 1;
+        for(que[qt++] = t; qh++ < qt;){
+            int u = que[qh-1];
             for(edge& e : G[u]){
-                if(potential[e.to] == V && G[e.to][e.rev].cap > 0 && e.to != s){
-                    potential[e.to] = h, que[qt++] = e.to;
+                if(potential[e.to] == V && G[e.to][e.rev].cap > 0){
+                    potential[e.to] = potential[u] + 1, que[qt++] = e.to;
                 }
             }
         }
+    }
+    int init(){
+        potential[s] = V+1;
+        bfs();
         for(edge& e : G[s]){
             if(potential[e.to] < V){
                 G[e.to][e.rev].cap = e.cap, excess[s] -= e.cap, excess[e.to] += e.cap;
             }
             e.cap = 0;
         }
-        for(int i = 0; i < V; ++i){
-            if(potential[i] < V){
-                pot_max = (potential[i] > pot_max) ? potential[i] : pot_max;
-                all_ver.insert(potential[i], i);
-                if(excess[i] > 0 && i != t) act_ver.push(potential[i], i);
-            }
-        }
-        return pot_max;
+        return calc_active();
     }
     int global_relabel(){
-        fill(potential.begin(), potential.end(), V);
-        potential[t] = 0;
-        int qh = 0, qt = 0;
-        for(que[qt++] = t; qh < qt;){
-            int u = que[qh++], h = potential[u] + 1;
-            for(edge& e : G[u]){
-                if(potential[e.to] == V && G[e.to][e.rev].cap > 0 && e.to != s){
-                    potential[e.to] = h, que[qt++] = e.to;
-                }
-            }
-        }
+        bfs();
         all_ver.clear(), act_ver.clear();
-        pot_max = -1;
-        for(int i = 0; i < V; ++i){
-            if(potential[i] < V){
-                cur_edge[i] = 0;
-                pot_max = (potential[i] > pot_max) ? potential[i] : pot_max;
-                all_ver.insert(potential[i], i);
-                if(excess[i] > 0 && i != t) act_ver.push(potential[i], i);
-            }
-        }
-        return pot_max;
+        return calc_active();
     }
     void gap_relabel(const int u){
         for(int i = potential[u]; i <= pot_max; ++i){
             for(int id = all_ver.dat[V+i].next; id < V; id = all_ver.dat[id].next){
-                potential[id] = V;
+                potential[id] = V+1;
             }
             all_ver.sz[i] = 0;
             all_ver.dat[V+i].prev = all_ver.dat[V+i].next = V+i;
@@ -118,18 +113,18 @@ private:
         for(int& i = cur_edge[u]; i < (int)G[u].size(); ++i){
             edge& e = G[u][i];
             if(potential[u] == potential[e.to] + 1 && e.cap > 0){
-                push(u, e);
-                if(excess[u] == 0) return potential[u];
+                if(push(u, e)) return potential[u];
             }
         }
         return relabel(u);
     }
-    void push(const int u, edge& e){
+    bool push(const int u, edge& e){
         T f = min(e.cap, excess[u]);
         const int v = e.to;
         e.cap -= f, excess[u] -= f;
         G[v][e.rev].cap += f, excess[v] += f;
         if(excess[v] == f && v != t) act_ver.push(potential[v], v);
+        return (excess[u] == 0);
     }
     int relabel(const int u){
         ++checker;
@@ -143,10 +138,10 @@ private:
         }
         if((int)all_ver.size(prv) > 1){
             all_ver.erase(prv, u);
-            if((potential[u] = cur) == V) return prv;
+            if((potential[u] = cur) == V) return potential[u] = V+1, prv;
             act_ver.push(cur, u);
             all_ver.insert(cur, u);
-            pot_max = (pot_max > cur) ? pot_max : cur;
+            pot_max = max(pot_max, cur);
         }else{
             gap_relabel(u);
             return pot_max = prv - 1;
@@ -155,14 +150,14 @@ private:
     }
 public:
     vector<vector<edge> > G;
-    PushRelabel(int node_size)
+    PushRelabel(const int node_size)
         : V(node_size), pot_max(-1), checker(0), excess(V, (T)0),
-            potential(V, V), cur_edge(V, 0), que(V), all_ver(V, V), act_ver(V, V), G(V){}
-    void add_edge(int _from, int _to, T _cap){
+            potential(V), cur_edge(V), que(V), all_ver(V, V), act_ver(V, V), G(V){}
+    void add_edge(const int _from, const int _to, const T _cap){
         G[_from].emplace_back(_to, (int)G[_to].size(), _cap);
         G[_to].emplace_back(_from, (int)G[_from].size() - 1, 0);
     }
-    T solve(int source, int sink){
+    T solve(const int source, const int sink){
         s = source, t = sink;
         int level = init();
         while(level >= 0){
