722789da2801b4ee338a6c5a762c43fedebdb30d
kopricky <kopricky@yahoo.co.jp>
2019-11-22 03:43:00 +0900

    revise

--- Competitive_Programming/Graph_Decomposition/SCC.hpp
+++ Competitive_Programming/Graph_Decomposition/SCC.hpp
@@ -1,59 +1,57 @@
 #include "../header.hpp"
 
-// 有向グラフを強連結成分分解する(O(n))
-
+// 有向グラフを強連結成分分解する(O(n+m))
 class SCC {
+private:
+    const int V;
+    vector<vector<int> > G;
+    vector<int> ord, low;
+    stack<int> st;
+    void dfs(const int u, int& tm){
+        ord[u] = low[u] = tm++, st.push(u);
+        for(int v : G[u]){
+            if(ord[v] < 0){
+                dfs(v, tm);
+                low[u] = min(low[u], low[v]);
+            }else if(cmp[v] < 0){
+                low[u] = min(low[u], ord[v]);
+            }
+        }
+        if(ord[u] == low[u]){
+            while(true){
+                const int v = st.top();
+                st.pop();
+                cmp[v] = cnt;
+                if(v == u) break;
+            }
+            ++cnt;
+        }
+    }
 public:
-	int V,cnt;
-	vector<vector<int> > G,rG,graph;
-	vector<int> vs,cmp;
-	vector<bool> used;
-	SCC(int node_size) : V(node_size), G(V), rG(V), cmp(V), used(V, false){}
-	void add_edge(int from,int to){
-		G[from].push_back(to);
-		rG[to].push_back(from);
-	}
-	void dfs(int u){
-		used[u] = true;
-		for(int v : G[u]){
-			if(!used[v]){
-				dfs(v);
-			}
-		}
-		vs.push_back(u);
-	}
-	void dfs(int u,const int k){
-		used[u] = true;
-		cmp[u] = k;
-		for(int v : rG[u]){
-			if(!used[v]){
-				dfs(v,k);
-			}
-		}
-	}
-	int solve(){ //強連結成分の数を返す
-		for(int i = 0; i < V; i++){
-			if(!used[i]){
-				dfs(i);
-			}
-		}
-		fill(used.begin(),used.end(),false);
-		cnt = 0;
-		for(int i=V-1;i>=0;i--){
-			if(!used[vs[i]]){
-				dfs(vs[i],cnt++);
-			}
-		}
-		return cnt;
-	}
-	void make_graph(){
-		graph.resize(cnt);
-		for(int i = 0; i < V; i++){
-			for(int v : G[i]){
-				if(cmp[i] != cmp[v]){
-					graph[cmp[i]].push_back(cmp[v]);
-				}
-			}
-		}
-	}
+    vector<vector<int> > dag_graph;
+    vector<int> cmp;
+    int cnt;
+    SCC(const int node_size)
+        : V(node_size), G(V), ord(V, -1), low(V), cmp(V, -1), cnt(0){}
+    void add_edge(const int from, const int to){
+        G[from].push_back(to);
+    }
+    int solve(){ // 強連結成分の数を返す
+        int tm = 0;
+        for(int i = 0; i < V; ++i){
+            if(ord[i] < 0) dfs(i, tm);
+        }
+        for(int i = 0; i < V; ++i) cmp[i] = cnt - 1 - cmp[i];
+        return cnt;
+    }
+    void make_graph(){
+        dag_graph.resize(cnt);
+        for(int i = 0; i < V; ++i){
+            for(int j : G[i]){
+                if(cmp[i] != cmp[j]){
+                    dag_graph[cmp[i]].push_back(cmp[j]);
+                }
+            }
+        }
+    }
 };
