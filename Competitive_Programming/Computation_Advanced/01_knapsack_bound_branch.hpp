cf8534c557dfb4913d69288f9aa15975b1ac3501
kopricky <kopricky@yahoo.co.jp>
2019-11-29 03:42:01 +0900

    revise

--- Competitive_Programming/Computation_Advanced/01_knapsack_bound_branch.hpp
+++ Competitive_Programming/Computation_Advanced/01_knapsack_bound_branch.hpp
@@ -1,50 +1,65 @@
 #include "../header.hpp"
 
 //01ナップザック問題に対する分枝限定法を用いたアルゴリズム
-void dfs(int n, long long W, long long value, long long weight, long long& opt, int index, vector<pair<long long,long long> >& vec)
+void dfs(const int n, const long long W, const long long value, const long long weight, long long& opt,
+        const int index, const vector<long long>& rsumw, const vector<int>& rminw, const vector<pair<int, int> >& vec)
 {
-    if(index == (int)vec.size()){
+    if(index == n){
         opt = max(opt, value);
         return;
     }
+    if(rsumw[index] <= W - weight){
+        long long tvalue = value;
+        for(int i = index; i < n; ++i){
+            tvalue += vec[i].first;
+        }
+        opt = max(opt, tvalue);
+        return;
+    }
+    if(rminw[index] > W - weight) return;
     //使う
     if(weight + vec[index].second <= W){
         opt = max(opt, value + vec[index].first);
-        long long tweight = weight + vec[index].second;
-        long long tvalue = value + vec[index].first;
-        for(int i=index+1;i<(int)vec.size();i++){
+        long long tweight = weight + vec[index].second, tvalue = value + vec[index].first;
+        for(int i = index + 1; i < n; ++i){
             if(tweight + vec[i].second <= W){
-                tweight += vec[i].second;
-                tvalue += vec[i].first;
+                tweight += vec[i].second, tvalue += vec[i].first;
             }else{
                 tvalue += vec[i].first * (W - tweight) / vec[i].second;
                 break;
             }
         }
-        if(tvalue > opt){
-            dfs(n, W, value+vec[index].first,weight+vec[index].second,opt,index+1,vec);
+        if(opt < tvalue){
+            dfs(n, W, value + vec[index].first, weight + vec[index].second, opt, index + 1, rsumw, rminw, vec);
         }
     }
     //使わない
-    long long tweight = weight;
-    long long tvalue = value;
-    for(int i=index+1;i<(int)vec.size();i++){
+    long long tweight = weight, tvalue = value;
+    for(int i = index + 1; i < n; ++i){
         if(tweight + vec[i].second <= W){
-            tweight += vec[i].second;
-            tvalue += vec[i].first;
+            tweight += vec[i].second, tvalue += vec[i].first;
         }else{
             tvalue += vec[i].first * (W - tweight) / vec[i].second;
             break;
         }
     }
-    if(tvalue > opt){
-        dfs(n, W, value,weight,opt,index+1,vec);
+    if(opt < tvalue){
+        dfs(n, W, value, weight, opt, index + 1, rsumw, rminw, vec);
     }
 }
-//vecには(価値, 重さ)の順で商品を詰める
-long long solve(int n, long long W, vector<pair<long long,long long> >& vec)
+
+//vec には(価値, 重さ)の順で詰める
+long long solve(const int n, const long long W, vector<pair<int, int> >& vec)
 {
+    sort(vec.begin(), vec.end(), [&](const pair<int, int>& a, const pair<int, int>& b){
+        return (long long)a.first * b.second > (long long)a.second * b.first;
+    });
+    vector<long long> rsumw(n + 1, 0);
+    vector<int> rminw(n + 1, numeric_limits<int>::max());
+    for(int i = n - 1; i >= 0; --i){
+        rsumw[i] = rsumw[i + 1] + vec[i].second, rminw[i] = min(rminw[i + 1], vec[i].second);
+    }
     long long opt = 0;
-    dfs(n,W,0,0,opt,0,vec);
+    dfs(n, W, 0, 0, opt, 0, rsumw, rminw, vec);
     return opt;
 }
