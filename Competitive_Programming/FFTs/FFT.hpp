42b98c9aa828c90a4e0524fa255be493469e86c3
kopricky <kopricky@yahoo.co.jp>
2019-12-21 12:52:48 +0900

    revise

--- Competitive_Programming/FFTs/FFT.hpp
+++ Competitive_Programming/FFTs/FFT.hpp
@@ -1,44 +1,42 @@
 #include "../header.hpp"
 
-complex<double> operator* (const complex<double> a, const complex<double> b){
+complex<double> operator*(const complex<double> a, const complex<double> b){
     return complex<double>(a.real()*b.real()-a.imag()*b.imag(),a.real()*b.imag()+a.imag()*b.real());
 }
-
+ 
 vector<complex<double> > fft(vector<complex<double> > a,bool rev = false)
 {
     constexpr double PI = std::acos(-1);
-    int n = a.size(),h = 0;
-    for(int i = 0; 1 << i < n; i++) h++;
-    for(int i = 0; i < n; i++){
-        int j = 0;
-        for(int k = 0; k < h; k++){
-            j |= (i >> k & 1) << (h-1-k);
-        }
-        if (i < j) swap(a[i], a[j]);
-    }
-    for(int i = 1; i < n; i *= 2) {
-        for(int j = 0; j < i; j++){
-            complex<double> w = polar(1.0,2*PI/(i*2)*(rev?-1:1)*j);
-            for(int k = 0; k < n; k += i * 2) {
-                complex<double> s = a[j+k];
-                complex<double> t = a[j+k+i]*w;
-                a[j+k+0] = s+t;
-                a[j+k+i] = s-t;
-            }
-        }
-    }
+    unsigned int i, j, k, l;
+    double r;
+    complex<double> p, q, s;
+    const unsigned int size = a.size();
+	if(size == 1) return a;
+	vector<complex<double> > b(size);
+	r = rev ? (- 2 * PI / size) : 2 * PI / size;
+	s = polar(1.0, r);
+	vector<complex<double> > kp(size / 2 + 1, 1);
+	for(i = 0; i < size / 2; ++i) kp[i + 1] = kp[i] * s;
+	for(i = 1, l = size / 2; i < size; i <<= 1, l >>= 1){
+		for(j = 0, r = 0; j < l; ++j, r += i){
+            for(k = 0, s = kp[i * j]; k < i; ++k){
+				p = a[k + r], q = a[k + r + size / 2];
+				b[k + 2 * r] = p + q;
+				b[k + 2 * r + i] = (p - q) * s;
+			}
+		}
+		swap(a, b);
+	}
     if(rev){
-        for(int i = 0; i < n; i++){
-            a[i] /= n;
-        }
+        for(i = 0; i < size; i++){ a[i] /= size; }
     }
     return a;
 }
-
+ 
 vector<int> mul(vector<int> a, vector<int> b)
 {
     int s = (int)a.size() + (int)b.size() - 1,t = 1;
-    while (t < s) t *= 2;
+    while(t < s) t *= 2;
     vector<complex<double> > A(t), B(t);
     for(int i = 0; i < (int)a.size(); i++){
         A[i].real(a[i]);
--- Competitive_Programming/FFTs/NTT.hpp
+++ Competitive_Programming/FFTs/NTT.hpp
@@ -9,49 +9,63 @@
 // (469762049, 3)
 // (1224736769, 3)
 
-#define MOD 924844033
-#define root 5
+#define MOD 998244353
+#define root 3
 
-int add(const int x, const int y) { return (x + y < MOD) ? x + y : x + y - MOD; }
-int mul(const int x, const int y) { return (long long)x * y % MOD; }
-int power(int x, int n)
+unsigned int add(const unsigned int x, const unsigned int y)
 {
-    int res = 1;
-    while(n > 0) {
-        if (n & 1) { res = mul(res, x); }
+	return (x + y < MOD) ? x + y : x + y - MOD;
+}
+
+unsigned int sub(const unsigned int x, const unsigned int y)
+{
+	return (x >= y) ? (x - y) : (MOD - y + x);
+}
+
+unsigned int mul(const unsigned int x, const unsigned int y)
+{
+	return (unsigned long long)x * y % MOD;
+}
+
+unsigned int mod_pow(unsigned int x, unsigned int n)
+{
+    unsigned int res = 1;
+    while(n > 0){
+        if(n & 1){ res = mul(res, x); }
         x = mul(x, x);
         n >>= 1;
     }
     return res;
 }
-int inverse(const int x) { return power(x, MOD - 2); }
+
+unsigned int inverse(const unsigned int x)
+{
+	return mod_pow(x, MOD - 2);
+}
 
 void ntt(vector<int>& a, const bool rev = false)
 {
-    int i,j,k,s,t,v,w,wn;
-    const int size = (int)a.size();
-    const int height = (int)log2(2 * size - 1);
-    for(i = 0; i < size; i++) {
-        j = 0;
-        for(k = 0; k < height; k++) { j |= (i >> k & 1) << (height - 1 - k); }
-        if(i < j) swap(a[i], a[j]);
-    }
-    for(i = 1; i < size; i *= 2) {
-        w = power(root, (MOD - 1) / (i * 2));
-        if(rev) w = inverse(w);
-        for(j = 0; j < size; j += i * 2) {
-            wn = 1;
-            for(k = 0; k < i; k++) {
-                s = a[j + k + 0], t = mul(a[j + k + i], wn);
-                a[j + k + 0] = add(s, t);
-                a[j + k + i] = add(s, MOD - t);
-                wn = mul(wn, w);
-            }
-        }
-    }
+    unsigned int i, j, k, l, p, q, r, s;
+    const unsigned int size = a.size();
+	if(size == 1) return;
+	vector<int> b(size);
+	r = rev ? (MOD - 1 - (MOD - 1) / size) : (MOD - 1) / size;
+	s = mod_pow(root, r);
+	vector<unsigned int> kp(size / 2 + 1, 1);
+	for(i = 0; i < size / 2; ++i) kp[i + 1] = mul(kp[i], s);
+	for(i = 1, l = size / 2; i < size; i <<= 1, l >>= 1){
+		for(j = 0, r = 0; j < l; ++j, r += i){
+			for(k = 0, s = kp[i * j]; k < i; ++k){
+				p = a[k + r], q = a[k + r + size / 2];
+				b[k + 2 * r] = add(p, q);
+				b[k + 2 * r + i] = mul(sub(p, q), s);
+			}
+		}
+		swap(a, b);
+	}
     if(rev){
-        v = inverse(size);
-        for (i = 0; i < size; i++) { a[i] = mul(a[i], v); }
+        s = inverse(size);
+        for(i = 0; i < size; i++){ a[i] = mul(a[i], s); }
     }
 }
 
@@ -59,12 +73,12 @@ vector<int> convolute(const vector<int>& a, const vector<int>& b)
 {
     const int size = (int)a.size() + (int)b.size() - 1;
     int t = 1;
-    while (t < size) { t <<= 1; }
+    while(t < size){ t <<= 1; }
     vector<int> A(t, 0), B(t, 0);
-    for (int i = 0; i < (int)a.size(); i++) { A[i] = a[i]; }
-    for (int i = 0; i < (int)b.size(); i++) { B[i] = b[i]; }
+    for(int i = 0; i < (int)a.size(); i++){ A[i] = a[i]; }
+    for(int i = 0; i < (int)b.size(); i++){ B[i] = b[i]; }
     ntt(A), ntt(B);
-    for (int i = 0; i < t; i++) { A[i] = mul(A[i], B[i]); }
+    for (int i = 0; i < t; i++){ A[i] = mul(A[i], B[i]); }
     ntt(A, true);
     A.resize(size);
     return A;
