9cfad0d3b2a27d956fe19fab5bdda555127f1db2
kopricky <kopricky@yahoo.co.jp>
2020-01-23 00:00:46 +0900

    revise

--- Competitive_Programming/Graph_Decomposition/articulation_node.hpp
+++ Competitive_Programming/Graph_Decomposition/articulation_node.hpp
@@ -1,7 +1,6 @@
 #include "../header.hpp"
 
 //関節点を求めるアルゴリズム(計算量は線形)
-//各2(点)連結成分を求めたいときはコメントアウトをはずす
 class Articulation {
 public:
 	using P = pair<int, int>;
@@ -10,8 +9,6 @@ public:
 	vector<int> ord;	//訪問時間
 	vector<int> low; //low[u]はuから高々1個の後退辺を通ってたどりつけるノードのordの最小値
 	vector<bool> art; //関節点かどうかの判定
-	// vector<vector<P> > edgeset;
-	// stack<P> st;
 	Articulation(const int node_size)
 	 	: V(node_size), G(V), ord(V, -1), low(V), art(V, false){}
 	void add_edge(const int a, const int b){
@@ -21,21 +18,11 @@ public:
 		ord[u] = low[u] = tm++;
 		int ct = 0;	// ノードvの次数
 		for(int v : G[u]){
-			// if(v != p && ord[v] < ord[u]) st.push(P(u, v));
 			if(ord[v] < 0){
 				++ct;
 				dfs(v, u, tm);
 				low[u] = min(low[u], low[v]);	//子のノードのlowと比較する
 				if(p >= 0 && ord[u] <= low[v]) art[u] = true;
-				// if(low[v] >= ord[u]){
-				//     edgeset.push_back(vector<P>());
-				//     while(true){
-				//         P p = st.top();
-				//         st.pop();
-				//         edgeset.back().push_back(p);
-				//         if(p == P(u, v)) break;
-				//     }
-				// }
 			}else{
 				low[u] = min(low[u], ord[v]);
 			}
