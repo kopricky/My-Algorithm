9625475c58e83336346baca4d8b29ca58f099a4c
kopricky <kopricky@yahoo.co.jp>
2020-01-28 08:52:41 +0900

    revise

--- Competitive_Programming/DataStructure_On_Graph/merge_tech.hpp
+++ Competitive_Programming/DataStructure_On_Graph/merge_tech.hpp
@@ -1,14 +1,14 @@
 #include "../header.hpp"
 
 // "データ構造をマージする一般的なテク"と呼ばれるもの
-// 大きい方に小さい方をmergeすれば全体でO(nlog(n))という話
+// 大きい方に小さい方を merge すれば全体で O(n log n) という話
 
 class MergeTech{
 private:
     int sz;
 public:
-    vector<int> par;              //所属しているグループ番号(par[x]はunionfindのfind(x)に対応)
-    vector<vector<int> > group;   //group[x]:グループxに含まれる数
+    vector<int> par;              //所属しているグループ番号(par[x] は union find の find(x) に対応)
+    vector<vector<int> > group;   //group[x]: グループ x に含まれる数
     MergeTech(){}
     MergeTech(int node_size) : sz(node_size), par(sz), group(sz){
         for(int i = 0; i < sz; i++){
