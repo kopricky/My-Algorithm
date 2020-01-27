e18d6eb36bde3407131ff1c065fdc548ea17247e
kopricky <kopricky@yahoo.co.jp>
2020-01-28 08:10:50 +0900

    revise

--- Competitive_Programming/DataStructure_On_Graph/partial_persistent_union_find.hpp
+++ Competitive_Programming/DataStructure_On_Graph/partial_persistent_union_find.hpp
@@ -6,33 +6,25 @@ public:
     int last;
     vector<pair<int, int> > par;
     vector<vector<pair<int, int> > > sz;
-    PartialPersistentUnionFind(int node_size)
+    PartialPersistentUnionFind(const int node_size)
         : V(node_size), last(numeric_limits<int>::min()), par(V), sz(V){
         for(int i = 0; i < V; ++i){
-            par[i] = {1, i};
+            par[i] = {numeric_limits<int>::max(), i};
             sz[i].emplace_back(numeric_limits<int>::min(), 1);
         }
     }
-    int find(const int u, const int _time){
-        if(u == par[u].second || par[u].first > _time){
-            return u;
-        }else{
-            return find(par[u].second, _time);
-        }
+    int find(int u, const int _time){
+        while(par[u].first <= _time) u = par[u].second;
+        return u;
     }
     bool unite(int u, int v, const int _time){
         assert(last <= _time);
-        if(same(u, v, _time)) return false;
         last = _time;
         u = find(u, _time), v = find(v, _time);
-        if(par[u].first < par[v].first){
-            swap(u, v);
-        }
+        if(u == v) return false;
+        if(sz[u].back().first < sz[v].back().first) swap(u, v);
         par[v] = {_time, u};
         sz[u].emplace_back(_time, sz[u].back().second + sz[v].back().second);
-        if(par[u].first == par[v].first){
-            ++par[u].first;
-        }
         return true;
     }
     bool same(const int u, const int v, const int _time){
@@ -41,6 +33,7 @@ public:
     int size(int u, const int _time){
         u = find(u, _time);
         return sz[u][static_cast<int>(upper_bound(sz[u].begin(), sz[u].end(),
-            make_pair(_time, V+1)) - sz[u].begin()) - 1].second;
+            make_pair(_time, V + 1)) - sz[u].begin()) - 1].second;
     }
 };
+
