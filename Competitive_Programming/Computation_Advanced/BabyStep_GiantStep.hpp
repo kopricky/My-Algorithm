75e1bf7132a8bdd0335a99fb05ce4eb5b9b2b6fd
kopricky <kopricky@yahoo.co.jp>
2020-02-09 01:39:05 +0900

    revise

--- Competitive_Programming/Computation_Advanced/BabyStep_GiantStep.hpp
+++ Competitive_Programming/Computation_Advanced/BabyStep_GiantStep.hpp
@@ -1,21 +1,22 @@
 #include "../header.hpp"
 
 //離散対数問題に対するアルゴリズム
-int mod_pow(int a, int b, int m)
+unsigned int mod_pow(unsigned int a, unsigned int b, unsigned int m)
 {
     a %= m;
-    int res = 1;
+    unsigned int res = 1;
     while(b){
         if(b & 1){
-            res = (long long)res * a % m;
+            res = (unsigned long long)res * a % m;
         }
-        a = (long long)a * a % m;
+        a = (unsigned long long)a * a % m;
         b >>= 1;
     }
     return res;
 }
 
-int mod_inv(int a, int m){
+unsigned int mod_inv(int a, int m)
+{
 	int u[] = {a, 1, 0}, v[] = {m, 0, 1}, t;
     while(*v){
 		t = *u / *v;
@@ -23,52 +24,54 @@ int mod_inv(int a, int m){
         swap(u[1] -= t * v[1], v[1]);
         swap(u[2] -= t * v[2], v[2]);
     }
-	return (u[1] % m + m) % m;
+    unsigned int res = u[1] % m + m;
+	return (res >= m) ? (res - m) : res;
 }
 
-int gcd(int a, int b){
-    int tmp;
+unsigned int gcd(unsigned int a, unsigned int b)
+{
+    unsigned int tmp;
     while(b) tmp = a, a = b, b = tmp % b;
     return a;
 }
 
 // a^x ≡ b (mod p) の解 x を求める (p^(1/2) log p)
-int baby_step_giant_step(int a, int b, int p)
+int baby_step_giant_step(unsigned int a, unsigned int b, unsigned int p)
 {
-    int m = ceil(sqrt(p));
-    unordered_map<int, int> mp;
-    int val = 1;
-    for(int i = 0; i < m; ++i){
+    unsigned int m = ceil(sqrt(p));
+    unordered_map<unsigned int, unsigned int> mp;
+    unsigned int val = 1;
+    for(unsigned int i = 0; i < m; ++i){
         if(mp.find(val) == mp.end()) mp[val] = i;
-        val = (long long)val * a % p;
+        val = (unsigned long long)val * a % p;
     }
-    int inv = mod_pow(mod_inv(a, p), m, p);
-    int cur = b, res = numeric_limits<int>::max();
-    for(int i = 0; i < m; ++i){
+    unsigned int inv = mod_pow(a, p - m - 1, p);
+    unsigned int cur = b, res = numeric_limits<unsigned int>::max();
+    for(unsigned int i = 0; i < m; ++i){
         auto it = mp.find(cur);
         if(it != mp.end()){
             res = min(res, i * m + it->second);
         }else{
-            cur = (long long)cur * inv % p;
+            cur = (unsigned long long)cur * inv % p;
         }
     }
-    return (res == numeric_limits<int>::max()) ? -1 : res;
+    return (res == numeric_limits<unsigned int>::max()) ? -1 : res;
 }
 
-int general_baby_step_giant_step(int a, int b, int p)
+int general_baby_step_giant_step(unsigned int a, unsigned int b, unsigned int p)
 {
     if(a == 0){
         if(b == 0) return (p == 1) ? 0 : 1;
         else if(b == 1) return 0;
         else return -1;
     }
-    int g, cnt = 0;
+    unsigned int g, cnt = 0;
     for(;;++cnt){
         if((b == 1) || (p == 1)) return cnt;
         if((g = gcd(a, p)) == 1) break;
         if(b % g != 0) return -1;
         b /= g, p /= g;
-        b = (long long)mod_inv(a / g, p) * b % p;
+        b = (unsigned long long)mod_inv(a / g, p) * b % p;
     }
     int res = baby_step_giant_step(a, b, p);
     return (res < 0) ? res : res + cnt;
--- Competitive_Programming/Computation_Basic/allxor.hpp
+++ Competitive_Programming/Computation_Basic/allxor.hpp
@@ -1,9 +1,6 @@
 #include "../header.hpp"
 
-//1~valまでの全xor
+// 0 ~ val までの全 xor
 long long allxor(long long val) {
-	int i;
-	long long ret=((val+1)/2)%2;
-	for(i=0;i<=60;i++) if((val>>i)%2) ret |= (1^(val%2))<<i;
-	return ret;
+	return (val & 1LL) ? ((val & 3) == 1) : (val ^ (((val ^ 1) & 3) == 3));
 }
