d3497f73ccba859fac1c235820c4731c14201832
kopricky <kopricky@yahoo.co.jp>
2020-03-29 14:38:26 +0900

    revise

--- Competitive_Programming/SegmentTrees/OrthogonalRangeReport.hpp
+++ Competitive_Programming/SegmentTrees/OrthogonalRangeReport.hpp
@@ -6,70 +6,6 @@
 // (フラクショナルカスケーディング版)時間計算量:構築O(nlog(n)),クエリO(log(n)+k)
 // 空間計算量:O(nlog(n))
 
-#define all(v) (v).begin(),(v).end()
-
-template<typename CandidateType> class OrthogonalRangeReport
-{
-private:
-    using CT = CandidateType;
-    using pcc = pair<CT, CT>;
-    using pci = pair<CT, int>;
-    int n, sz;
-    // x座標
-    vector<CT> xs;
-    // y座標, インデックス
-    vector<vector<pci> > ys;
-    void _query(int lxid, int rxid, const CT ly, const CT ry, vector<int>& report){
-        lxid += n, rxid += n;
-        while(lxid != rxid){
-            if(lxid & 1){
-                const auto st = lower_bound(all(ys[lxid]), pci(ly, -1));
-                const auto ed = lower_bound(all(ys[lxid]), pci(ry, -1));
-                for(auto it = st; it != ed; ++it) report.push_back(it->second);
-                ++lxid;
-            }
-            if(rxid & 1){
-                --rxid;
-                const auto st = lower_bound(all(ys[rxid]), pci(ly, -1));
-                const auto ed = lower_bound(all(ys[rxid]), pci(ry, -1));
-                for(auto it = st; it != ed; ++it) report.push_back(it->second);
-            }
-            lxid >>= 1, rxid >>= 1;
-        }
-    }
-public:
-    OrthogonalRangeReport(const vector<pcc>& cand) : n(1), sz((int)cand.size()), xs(sz){
-        while(n < sz){
-            n *= 2;
-        }
-        vector<pci> sorted(sz);
-        for(int i = 0; i < sz; ++i){
-            sorted[i] = {cand[i].first, i};
-        }
-        sort(sorted.begin(), sorted.end());
-        ys.resize(2*n);
-        for(int i = 0; i < sz; ++i){
-            xs[i] = sorted[i].first;
-            ys[i+n] = {make_pair(cand[sorted[i].second].second, sorted[i].second)};
-        }
-        for(int i = n-1; i >= 1; --i){
-            ys[i].resize((int)ys[2*i].size() + (int)ys[2*i+1].size());
-            merge(all(ys[2*i]), all(ys[2*i+1]), ys[i].begin(), [&](const pci& a, const pci& b){
-                return a.first < b.first;
-            });
-        }
-    }
-    void query(const CT lx, const CT ly, const CT rx, const CT ry, vector<int>& report) {
-        const int lxid = lower_bound(all(xs), lx) - xs.begin();
-        const int rxid = lower_bound(all(xs), rx) - xs.begin();
-        if(lxid >= rxid) return;
-        _query(lxid, rxid, ly, ry, report);
-    }
-};
-
-// フラクショナルカスケーディング版
-#define all(v) (v).begin(), (v).end()
-
 template<typename CandidateType> class OrthogonalRangeReport
 {
 private:
@@ -82,10 +18,9 @@ private:
     // y座標, インデックス
     vector<vector<pci> > ys;
     // 位置
-    vector<vector<vector<int> > > pos;
+    vector<vector<array<int, 2> > > pos;
     static bool less(const pci& a, const pci& b, const vector<pcc>& cand){
-        return (a.first == b.first) ?
-                (cand[a.second].first < cand[b.second].first) : (a.first < b.first);
+        return (a.first == b.first) ? (cand[a.second].first < cand[b.second].first) : (a.first < b.first);
     }
     void query(const int lxid, const int rxid, const int lyid, const int ryid,
                 vector<int>& report, const int k, const int l, const int r){
@@ -115,10 +50,9 @@ public:
         for(int i = n-2; i >= 0; --i){
             ys[i].resize((int)ys[2*i+1].size() + (int)ys[2*i+2].size());
             merge(all(ys[2*i+1]), all(ys[2*i+2]), ys[i].begin(), [&](const pci& a, const pci& b){
-                return (a.first == b.first) ?
-                        (cand[a.second].first < cand[b.second].first) : (a.first < b.first);
+                return (a.first == b.first) ? (cand[a.second].first < cand[b.second].first) : (a.first < b.first);
             });
-            pos[i].resize((int)ys[i].size() + 1, vector<int>(2));
+            pos[i].resize((int)ys[i].size() + 1);
             for(int k = 0; k < 2; ++k){
                 int p = 0;
                 for(int j = 0; j < (int)ys[i].size(); ++j){
