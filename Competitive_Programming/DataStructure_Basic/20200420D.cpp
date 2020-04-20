ec4e498e5fd8b63606f662d7ae066268363c1bb5
kopricky <kopricky@yahoo.co.jp>
2020-04-20 20:19:05 +0900

    revise

--- Competitive_Programming/DataStructure_Basic/20200420D.cpp
+++ Competitive_Programming/DataStructure_Basic/20200420D.cpp
@@ -5,8 +5,45 @@
 
 using namespace std;
 
+const int MAX_N = 400005;
+
+class BIT {
+private:
+    int n;
+    vector<int> bit;
+public:
+    void add(int i, int x){
+        ++i;
+        while(i < n) bit[i] += x, i += i & -i;
+    }
+    int sum(int i){
+        ++i;
+        int s = 0;
+        while(i > 0) s += bit[i], i -= i & -i;
+        return s;
+    }
+    int upper_bound(int x){
+        int b = 1;
+        while(b < n) b *= 2;
+        int a = 0;
+        while(b > 0){
+            if(a + b < n - 1 && x >= bit[a + b]){
+                x -= bit[a + b], a += b;
+            }
+            b >>= 1;
+        }
+        return (a < b) ? a : -1;
+    }
+    BIT(const int sz) : n(sz + 1), bit(n, 0){}
+};
+
+vector<pair<int, int> > info[MAX_N];
+
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    
+    int n;
+    rep(i,n){
+        cin >> u >> v;
+    }
 }
