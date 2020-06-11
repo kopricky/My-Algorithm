b3c25ac0a10104b8ed7eb427013a50974ce0abeb
kopricky <kopricky@yahoo.co.jp>
2020-06-11 18:40:24 +0900

    revise

--- Competitive_Programming/FFTs_polynomial/FFT.hpp
+++ Competitive_Programming/FFTs_polynomial/FFT.hpp
@@ -12,22 +12,22 @@ void fft(vector<complex<double> >& a,bool rev = false)
     double r;
     complex<double> p, q, s;
     const unsigned int size = a.size();
-	if(size == 1) return;
-	vector<complex<double> > b(size);
-	r = rev ? (- 2 * PI / size) : 2 * PI / size;
-	s = polar(1.0, r);
-	vector<complex<double> > kp(size / 2 + 1, 1);
-	for(i = 0; i < size / 2; ++i) kp[i + 1] = kp[i] * s;
-	for(i = 1, l = size / 2; i < size; i <<= 1, l >>= 1){
-		for(j = 0, r = 0; j < l; ++j, r += i){
+    if(size == 1) return;
+    vector<complex<double> > b(size);
+    r = rev ? (- 2 * PI / size) : 2 * PI / size;
+    s = polar(1.0, r);
+    vector<complex<double> > kp(size / 2 + 1, 1);
+    for(i = 0; i < size / 2; ++i) kp[i + 1] = kp[i] * s;
+    for(i = 1, l = size / 2; i < size; i <<= 1, l >>= 1){
+        for(j = 0, r = 0; j < l; ++j, r += i){
             for(k = 0, s = kp[i * j]; k < i; ++k){
-				p = a[k + r], q = a[k + r + size / 2];
-				b[k + 2 * r] = p + q;
-				b[k + 2 * r + i] = (p - q) * s;
-			}
-		}
-		swap(a, b);
-	}
+                p = a[k + r], q = a[k + r + size / 2];
+                b[k + 2 * r] = p + q;
+                b[k + 2 * r + i] = (p - q) * s;
+            }
+        }
+        swap(a, b);
+    }
     if(rev){
         for(i = 0; i < size; i++){ a[i] /= size; }
     }
