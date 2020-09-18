737cf86d841e6eb34e5d12a8c8c6d23ad8e9b415
kopricky <kopricky@yahoo.co.jp>
2020-09-19 00:32:19 +0900

    revise

--- Competitive_Programming/FFTs_polynomial/FFT.hpp
+++ Competitive_Programming/FFTs_polynomial/FFT.hpp
@@ -7,7 +7,7 @@ complex<double> operator*(const complex<double> a, const complex<double> b)
 
 void fft(vector<complex<double> >& a,bool rev = false)
 {
-    constexpr double PI = std::acos(-1);
+    const double PI = 3.14159265358979324;
     unsigned int i, j, k, l;
     double r;
     complex<double> p, q, s;
@@ -15,15 +15,13 @@ void fft(vector<complex<double> >& a,bool rev = false)
     if(size == 1) return;
     vector<complex<double> > b(size);
     r = rev ? (- 2 * PI / size) : 2 * PI / size;
-    s = polar(1.0, r);
     vector<complex<double> > kp(size / 2 + 1, 1);
-    for(i = 0; i < size / 2; ++i) kp[i + 1] = kp[i] * s;
+    for(i = 0; i <= size / 2; ++i) kp[i] = polar((double)1.0, i * r);
     for(i = 1, l = size / 2; i < size; i <<= 1, l >>= 1){
         for(j = 0, r = 0; j < l; ++j, r += i){
             for(k = 0, s = kp[i * j]; k < i; ++k){
                 p = a[k + r], q = a[k + r + size / 2];
-                b[k + 2 * r] = p + q;
-                b[k + 2 * r + i] = (p - q) * s;
+                b[k + 2 * r] = p + q, b[k + 2 * r + i] = (p - q) * s;
             }
         }
         swap(a, b);
