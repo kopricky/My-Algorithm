6d4af085dc5bfa7e2135552a3ebac95196f314a3
kopricky <kopricky@yahoo.co.jp>
2020-12-17 17:48:26 +0900

    revise

--- Competitive_Programming/Geometry/line.hpp
+++ Competitive_Programming/Geometry/line.hpp
@@ -2,21 +2,11 @@
 
 // 直線の管理, 座標の値は int 型を仮定
 
-int gcd(int a, int b)
-{
-    if(!b) return a;
-    if(a % b == 0){
-        return b;
-    }else{
-        return gcd(b,a%b);
-    }
-}
-
 class line
 {
 public:
     int a, b;
-    ll c;
+    long long c;
     line(){}
     line(int a_, int b_, ll c_) : a(a_), b(b_), c(c_){}
     // (p,q), (r,s) を結ぶ直線
@@ -26,14 +16,7 @@ public:
 		}else if(q == s){
 			a = 0, b = 1, c = -q;
 		}else{
-			int dx = r-p, dy = s-q;
-			int g = gcd(abs(dx), abs(dy));
-			if((ll)dx * dy < 0){
-				a = abs(dy)/g, b = abs(dx)/g;
-			}else{
-				a = -abs(dy)/g, b = abs(dx)/g;
-			}
-			c = -(ll)a*p-(ll)b*q;
+            a = s - q, b = -(r - p), c = (long long)q * r - (long long)p * s;
 		}
     }
     line(P x, P y) : line(x.first, x.second, y.first, y.second){}
@@ -51,7 +34,7 @@ public:
     }
     // 点(p, q) が直線上にあるか
     inline bool onLine(int p, int q){
-        return ((ll)a*p+(ll)b*q+c == 0);
+        return ((long long)a * p + (long long)b * q + c == 0);
     }
     inline bool onLine(P x){ return onLine(x.first, x.second); }
 };
