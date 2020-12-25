bb87c32bad53623ce2f42def6d62003052309c8d
kopricky <kopricky@yahoo.co.jp>
2020-12-26 07:02:53 +0900

    revise

--- Competitive_Programming/Matrix/matrix_mod.hpp
+++ Competitive_Programming/Matrix/matrix_mod.hpp
@@ -1,5 +1,74 @@
 #include "../header.hpp"
-#define EPS 1e-10
+
+#define MOD 1000000007
+
+template <unsigned int mod>
+class ModInt {
+private:
+    unsigned int v;
+    static unsigned int norm(const unsigned int& x){ return x < mod ? x : x - mod; }
+    static ModInt make(const unsigned int& x){ ModInt m; return m.v = x, m; }
+    static ModInt inv(const ModInt& x){ return make(inverse(x.v, mod)); }
+    static unsigned int inverse(int a, int m){
+        int u[] = {a, 1, 0}, v[] = {m, 0, 1}, t;
+        while(*v){
+            t = *u / *v;
+            swap(u[0] -= t * v[0], v[0]), swap(u[1] -= t * v[1], v[1]), swap(u[2] -= t * v[2], v[2]);
+        }
+        return (u[1] % m + m) % m;
+    }
+
+public:
+    ModInt() : v{0}{}
+    ModInt(const long long val) : v{norm(val % mod + mod)} {}
+    ModInt(const ModInt<mod>& n) : v{n()} {}
+    explicit operator bool() const noexcept { return v != 0; }
+    bool operator!() const noexcept { return !static_cast<bool>(*this); }
+    ModInt& operator=(const ModInt& n){ return v = n(), (*this); }
+    ModInt& operator=(const long long val){ return v = norm(val % mod + mod), (*this); }
+    ModInt operator+() const { return *this; }
+    ModInt operator-() const { return v == 0 ? make(0) : make(mod - v); }
+    ModInt operator+(const ModInt& val) const { return make(norm(v + val())); }
+    ModInt operator-(const ModInt& val) const { return make(norm(v + mod - val())); }
+    ModInt operator*(const ModInt& val) const { return make((long long)v * val() % mod); }
+    ModInt operator/(const ModInt& val) const { return *this * inv(val); }
+    ModInt& operator+=(const ModInt& val){ return *this = *this + val; }
+    ModInt& operator-=(const ModInt& val){ return *this = *this - val; }
+    ModInt& operator*=(const ModInt& val){ return *this = *this * val; }
+    ModInt& operator/=(const ModInt& val){ return *this = *this / val; }
+    ModInt operator+(const long long val) const { return ModInt{v + val}; }
+    ModInt operator-(const long long val) const { return ModInt{v - val}; }
+    ModInt operator*(const long long val) const { return ModInt{(long long)(v * (val % mod))}; }
+    ModInt operator/(const long long val) const { return ModInt{(long long)v * inv(val)}; }
+    ModInt& operator+=(const long long val){ return *this = *this + val; }
+    ModInt& operator-=(const long long val){ return *this = *this - val; }
+    ModInt& operator*=(const long long val){ return *this = *this * val; }
+    ModInt& operator/=(const long long val){ return *this = *this / val; }
+    bool operator==(const ModInt& val) const { return v == val.v; }
+    bool operator!=(const ModInt& val) const { return !(*this == val); }
+    bool operator==(const long long val) const { return v == norm(val % mod + mod); }
+    bool operator!=(const long long val) const { return !(*this == val); }
+    unsigned int operator()() const { return v; }
+    friend ModInt operator+(const long long val, const ModInt& n) { return n + val; }
+    friend ModInt operator-(const long long val, const ModInt& n) { return ModInt{val - n()}; }
+    friend ModInt operator*(const long long val, const ModInt& n) { return n * val; }
+    friend ModInt operator/(const long long val, const ModInt& n) { return ModInt{val} / n; }
+    friend bool operator==(const long long val, const ModInt& n) { return n == val; }
+    friend bool operator!=(const long long val, const ModInt& n) { return !(val == n); }
+    friend istream& operator>>(istream& is, ModInt& n){
+        unsigned int v;
+        return is >> v, n = v, is;
+    }
+    friend ostream& operator<<(ostream& os, const ModInt& n){ return (os << n()); }
+    friend ModInt mod_pow(ModInt x, long long n){
+        ModInt ans = ((mod == 1) ? 0 : 1);
+        while(n){
+            if(n & 1) ans *= x;
+            x *= x, n >>= 1;
+        }
+        return ans;
+    }
+};
 
 template<typename T> class mat : public vector<vector<T> > {
 private:
@@ -109,22 +178,25 @@ public:
         for(int i = 0; i < c; i++){
             if(res == r) return res;
             int pivot = res;
-            for(int j = res + 1; j < r; j++){
-                if(abs(B[j][i]) > abs(B[pivot][i])){
+            for(int j = res; j < r; j++){
+                if(B[j][i]){
                     pivot = j;
+                    break;
                 }
             }
-            if(abs(B[pivot][i]) < EPS) continue;
-            swap(B[pivot],B[res]);
+            if(!B[pivot][i]) continue;
+            swap(B[pivot], B[res]);
+            const T d = (T)1 / B[res][i];
             for(int j = i + 1; j < c; j++){
-                B[res][j] /= B[res][i];
+                B[res][j] *= d;
             }
             for(int j = res + 1; j < r; j++){
+                if(!B[j][i]) continue;
                 for(int k = i + 1; k < c; k++){
                     B[j][k] -= B[res][k] * B[j][i];
                 }
             }
-            res++;
+            ++res;
         }
         return res;
     }
@@ -142,17 +214,23 @@ public:
         }
         for(int i = 0; i < c; i++){
             int pivot = i;
-            for(int j = i + 1; j < r; j++){
-                if(abs(B[j][i]) > abs(B[pivot][i])){
+            for(int j = i; j < r; j++){
+                if(B[j][i]){
                     pivot = j;
+                    break;
                 }
             }
-            if(abs(B[pivot][i]) < EPS) return (T)0;
+            if(!B[pivot][i]) return (T)0;
             if(pivot != i) swap(B[i], B[pivot]), ans = -ans;
             ans *= B[i][i];
+            const T d = (T)1 / B[i][i];
+            for(int j = i + 1; j < c; j++){
+                B[i][j] *= d;
+            }
             for(int j = i + 1; j < r; j++){
-                for(int k = c - 1; k >= i; k--){
-                    B[j][k] -= B[i][k] * B[j][i] / B[i][i];
+                if(!B[j][i]) continue;
+                for(int k = i + 1; k < c; k++){
+                    B[j][k] -= B[i][k] * B[j][i];
                 }
             }
         }
@@ -175,23 +253,24 @@ public:
         for(int i = 0; i < r; i++){
             int pivot = i;
             for(int j = i; j < r; j++){
-                if(abs(B[j][i]) > abs(B[pivot][i])){
+                if(B[j][i]){
                     pivot = j;
+                    break;
                 }
             }
-            if(abs(B[pivot][i]) < EPS){
+            if(!B[pivot][i]){
                 cout << "正則でない" << endl;
                 exit(1);
             }
-            swap(B[i],B[pivot]);
+            swap(B[i], B[pivot]);
+            const T d = (T)1 / B[i][i];
             for(int j = i + 1; j < 2*r; j++){
-                B[i][j] /= B[i][i];
+                B[i][j] *= d;
             }
             for(int j = 0; j < r; j++){
-                if(i != j){
-                    for(int k = i + 1; k < 2*r; k++){
-                        B[j][k] -= B[j][i] * B[i][k];
-                    }
+                if(j == i || !B[j][i]) continue;
+                for(int k = i + 1; k < 2*r; k++){
+                    B[j][k] -= B[i][k] * B[j][i];
                 }
             }
         }
@@ -230,23 +309,25 @@ template<typename T> vector<T> eq_solve(const mat<T>& A, const vector<T>& b){
     }
     for(int i = 0; i < n; i++){
         int pivot = i;
-        for(int j = i; j < n;j++){
-            if(abs(B[j][i]) > abs(B[pivot][i])){
+        for(int j = i; j < n; j++){
+            if(B[j][i]){
                 pivot = j;
+                break;
             }
         }
-        if(abs(B[pivot][i]) < EPS){
+        if(!B[pivot][i]){
             cout << "解なしor不定" << endl;
             exit(1);
         }
         swap(B[i], B[pivot]);
+        const T d = (T)1 / B[i][i];
         for(int j = i + 1; j <= n; j++){
-            B[i][j] /= B[i][i];
+            B[i][j] *= d;
         }
         for(int j = 0; j < n; j++){
             if(i != j){
                 for(int k = i + 1; k <= n; k++){
-                    B[j][k] -= B[j][i] * B[i][k];
+                    B[j][k] -= B[i][k] * B[j][i];
                 }
             }
         }
@@ -269,9 +350,13 @@ template<typename T> mat<T> pow(mat<T> A, long long cnt)
         B[i][i] = 1;
     }
     while(cnt > 0){
-        if(cnt & 1) B = B * A;
+		if(cnt & 1) B = B * A;
         A = A * A;
         cnt >>= 1;
     }
     return B;
 }
+
+using mod = ModInt<MOD>;
+
+// 宣言 mat<mod> A(n, m); 
