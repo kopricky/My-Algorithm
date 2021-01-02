286bbbaf1f1b896366d944e69d80b37a5c3259c1
kopricky <kopricky@yahoo.co.jp>
2021-01-02 14:21:28 +0900

    revise

--- Competitive_Programming/DataStructure_Basic/SparseTable.hpp
+++ Competitive_Programming/DataStructure_Basic/SparseTable.hpp
@@ -6,22 +6,22 @@ private:
     vector<int> LogTable;
     vector<vector<T> > Table; //最小値を保持
 public:
-    SparseTable(vector<T>& v) : sz((int)v.size()), LogTable(sz+1){
+    SparseTable(const vector<T>& v) : sz((int)v.size()), LogTable(sz + 1){
         for(int i = 2; i < sz + 1; i++){
             LogTable[i] = LogTable[i >> 1] + 1;
         }
-        Table.resize(sz,vector<T>(LogTable[sz]+1));
-        for(int i = 0; i < sz; i++){
-            Table[i][0] = v[i];
+        Table.resize(LogTable[sz]+1, vector<T>(sz));
+        rep(i,sz){
+            Table[0][i] = v[i];
         }
         for(int k = 1; (1 << k) <= sz; k++){
             for(int i = 0; i + (1 << k) <= sz; i++){
-                Table[i][k] = min(Table[i][k-1],Table[i + (1 << (k-1))][k-1]);
+                Table[k][i] = min(Table[k-1][i], Table[k-1][i + (1 << (k-1))]);
             }
         }
     }
-    T query(int l,int r){
-        int k = LogTable[r-l];
-        return min(Table[l][k],Table[r-(1<<k)][k]);
+    T query(const int l, const int r){
+        const int k = LogTable[r-l];
+        return min(Table[k][l], Table[k][r-(1<<k)]);
     }
 };
--- Competitive_Programming/DataStructure_Basic/SparseTable_index.hpp
+++ Competitive_Programming/DataStructure_Basic/SparseTable_index.hpp
@@ -3,25 +3,25 @@
 template<typename T> class SparseTable {
 private:
     int sz;
-    vector<int> logTable;
-    vector<vector<pair<T, int> > > table; //最小値のインデックスを保持
+    vector<int> LogTable;
+    vector<vector<pair<T, int> > > Table; //最小値を保持
 public:
-    SparseTable(vector<T>& v) : sz((int)v.size()), logTable(sz+1){
+    SparseTable(const vector<T>& v) : sz((int)v.size()), LogTable(sz + 1){
         for(int i = 2; i < sz + 1; i++){
-            logTable[i] = logTable[i >> 1] + 1;
+            LogTable[i] = LogTable[i >> 1] + 1;
         }
-        table.resize(sz, vector<pair<T, int> >(logTable[sz]+1));
-        for(int i = 0; i < sz; i++){
-            table[i][0] = make_pair(v[i], i);
+        Table.resize(LogTable[sz]+1, vector<pair<T, int> >(sz));
+        rep(i,sz){
+            Table[0][i] = make_pair(v[i], i);
         }
         for(int k = 1; (1 << k) <= sz; k++){
             for(int i = 0; i + (1 << k) <= sz; i++){
-                table[i][k] = min(table[i][k-1], table[i + (1 << (k-1))][k-1]);
+                Table[k][i] = min(Table[k-1][i], Table[k-1][i + (1 << (k-1))]);
             }
         }
     }
-    pair<T, int> query(int l,int r){
-        int k = logTable[r-l];
-        return min(table[l][k], table[r-(1<<k)][k]);
+    pair<T, int> query(int l, int r){
+        int k = LogTable[r-l];
+        return min(Table[k][l], Table[k][r-(1<<k)]);
     }
 };
