002e509d00f51cfebfdd84d488200c60eccc2829
kopricky <kopricky@yahoo.co.jp>
2021-02-18 08:40:07 +0900

    revise

--- Competitive_Programming/FFTs_polynomial/FWHT.hpp
+++ Competitive_Programming/FFTs_polynomial/FWHT.hpp
@@ -1,30 +1,27 @@
 #include "../header.hpp"
 
-//高速Walsh–Hadamard変換(高速Fourier変換の一般化)
-//具体的には xor, or, and convolution の実装
-//xor, or, and については Walsh-Hadamard 行列が具体的に知られているので実装は容易
-//問題にするならMODで出そうなのでMODをとった実装になっています
+// 高速Walsh–Hadamard変換(高速Fourier変換の一般化)
+// 具体的には xor, or, and convolution の実装
+// xor, or, and については Walsh-Hadamard 行列が具体的に知られているので実装は容易
+// 問題にするならMODで出そうなのでMODをとった実装になっています
 
-#define MOD 1000000007
+#define MOD 998244353
 
-inline int add(int x, int y)
-{
-    return (x + y)%MOD;
+unsigned int add(unsigned int x, unsigned int y){
+    return (x + y >= MOD) ? (x + y - MOD) : (x + y);
 }
 
-inline int sub(int x, int y)
-{
-    return (x+MOD-y)%MOD;
+unsigned int sub(unsigned int x, unsigned int y){
+    return (x < y) ? (x + MOD - y) : (x - y);
 }
 
-inline int mul(int x, int y)
-{
-    return (long long)x*y%MOD;
+
+unsigned int mul(unsigned int x, unsigned int y){
+    return (unsigned long long)x * y % MOD;
 }
 
-inline int mod_pow(int a, int b)
-{
-    int res = 1;
+unsigned int mod_pow(unsigned int a, unsigned int b){
+    unsigned int res = 1;
     while(b){
         if(b & 1){
             res = mul(res, a);
@@ -36,23 +33,36 @@ inline int mod_pow(int a, int b)
 }
 
 //xor convolution
-void fwht(vector<int>& poly, bool rev=false){
+void fwht(vector<unsigned int>& poly, bool rev=false){
+    unsigned int u, v;
     int n = (int)poly.size();
     for(int i = 1; i < n; i *= 2){
-        for(int j = 0; j < i; j++){
-            for(int k = 0; k < n; k += i*2){
-                int u = poly[j+k], v = poly[j+k+i];
-                poly[j+k] = add(u, v);
-                poly[j+k+i] = sub(u, v);
+        for(int j = 0; j < i; ++j){
+            for(int k = 0; k < n; k += 2 * i){
+                u = poly[j + k], v = poly[j + k + i];
+                poly[j + k] = add(u, v), poly[j + k + i] = sub(u, v);
             }
         }
     }
     if(rev){
-        int inv = mod_pow(n, MOD-2);
+        unsigned int inv = mod_pow(n, MOD - 2);
         for(int i = 0; i < n; i++){
             poly[i] = mul(poly[i], inv);
         }
     }
+}
+
+vector<unsigned int> mul(vector<unsigned int> a, vector<unsigned int> b){
+    int sm = (int)a.size() + (int)b.size() - 1, size_ = 1;
+    while(size_ < sm) size_ *= 2;
+    a.resize(size_, 0), b.resize(size_, 0);
+    fwht(a), fwht(b);
+    for(int i = 0; i < size_; ++i){
+        a[i] = mul(a[i], b[i]);
+    }
+    fwht(a, true);
+    a.resize(sm);
+    return a;
 }
 
 //or convolution
